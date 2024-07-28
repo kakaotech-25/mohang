c94c7ac304f2f609af520eca0ef844df4d4a437d
Haon <88240193+msung99@users.noreply.github.com>
2024-07-28 18:11:13 +0900

    #34 test: 만료된 엑세스 토큰을 전달받으면 예외가 발생하는지 테스트

--- backend/src/test/java/moheng/auth/domain/JwtTokenProviderTest.java
+++ backend/src/test/java/moheng/auth/domain/JwtTokenProviderTest.java
@@ -73,6 +73,18 @@ public class JwtTokenProviderTest {
                 .isInstanceOf(InvalidTokenException.class);
     }
 
+    @DisplayName("만료된 엑세스 토큰을 전달받으면 예외를 발생시킨다.")
+    @Test
+    void 만료된_엑세스_토큰을_전달받으면_예외를_발생시킨다() {
+        // given
+        JwtTokenProvider expiredJwtTokenProvider = new JwtTokenProvider(SECRET_KEY, 0, 0);
+        String expiredToken = expiredJwtTokenProvider.createAccessToken("1L");
+
+        // when, then
+        assertThatThrownBy(() -> jwtTokenProvider.validateToken(expiredToken))
+                .isInstanceOf(InvalidTokenException.class);
+    }
+
     @DisplayName("변조되었거나 문제가 발생한 토큰을 전달받으면 예외가 발생한다.")
     @Test
     void 변조되었거나_문제가_발생한_토큰을_전달받으면_예외가_발생한다() {
