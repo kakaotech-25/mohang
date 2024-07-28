a225b632f2b0fe4ef9fb4e10f6c65384d6488f06
Haon <88240193+msung99@users.noreply.github.com>
2024-07-28 19:11:35 +0900

    #34 test: 유효한 멤버 id 로 리프레시 토큰을 조회하면 참을 리턴하는지 테스트

--- backend/src/test/java/moheng/auth/domain/InMemoryRefreshTokenRepositoryTest.java
+++ backend/src/test/java/moheng/auth/domain/InMemoryRefreshTokenRepositoryTest.java
@@ -25,11 +25,26 @@ public class InMemoryRefreshTokenRepositoryTest {
 
     @DisplayName("유효하지 않은 멤버 id 로 리프레시 토큰을 조회하면 거짓을 리턴한다.")
     @Test
-    void 유효하지_않은_멤버_id로_토큰을_조회하면_거짓을_리턴한다() {
+    void 유효하지_않은_멤버_id로_리프레시_토큰을_조회하면_거짓을_리턴한다() {
         // given
         long memberId = -1L;
 
         //when, then
         assertThat(refreshTokenRepository.existsById(memberId)).isFalse();
+    }
+
+    @DisplayName("유효한 멤버 id 로 리프레시 토큰을 조회하면 참을 리턴한다.")
+    @Test
+    void 유효한_멤버_id로_리프레시_토큰을_조회하면_참을_리턴한다() {
+        // given
+        long memberId = 1L;
+        String refreshToken = "refresh token";
+        refreshTokenRepository.save(memberId, refreshToken);
+
+        // when
+        boolean actual = refreshTokenRepository.existsById(memberId);
+
+        // then
+        assertThat(actual).isTrue();
     }
 }
