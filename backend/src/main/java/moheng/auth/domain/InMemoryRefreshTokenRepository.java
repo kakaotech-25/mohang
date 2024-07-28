646a1bb420dde91c983ed0a033053dabaae0c90c
Haon <88240193+msung99@users.noreply.github.com>
2024-07-28 19:00:22 +0900

    #34 feat: 인메모리 리프레시 토큰 저장소에 토큰을 저장하는 기능 구현

---
+++ backend/src/main/java/moheng/auth/domain/InMemoryRefreshTokenRepository.java
@@ -0,0 +1,19 @@
+package moheng.auth.domain;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class InMemoryRefreshTokenRepository {
+    private static final Map<Long, String> repository = new ConcurrentHashMap<>();
+
+    public void save(final long memberId, final String refreshToken) {
+        repository.put(memberId, refreshToken);
+    }
+
+    public boolean existsById(final long memberId) {
+        return repository.containsKey(memberId);
+    }
+}
--- backend/src/main/java/moheng/auth/domain/JwtTokenProvider.java
+++ backend/src/main/java/moheng/auth/domain/JwtTokenProvider.java
@@ -12,13 +12,17 @@ import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
+    private final InMemoryRefreshTokenRepository inMemoryRefreshTokenRepository;
     private final SecretKey secretKey;
     private final long accessTokenValidityInSeconds;
     private final long refreshTokenValidityInSeconds;
 
-    public JwtTokenProvider(@Value("${security.jwt.token.secret_key}") final String secretKey,
-                            @Value("${security.jwt.token.expire_length.access_token}") final long accessTokenValidityInSeconds,
-                            @Value("${security.jwt.token.expire_length.refresh_token}") final long refreshTokenValidityInSeconds) {
+    public JwtTokenProvider(
+            InMemoryRefreshTokenRepository inMemoryRefreshTokenRepository,
+            @Value("${security.jwt.token.secret_key}") final String secretKey,
+            @Value("${security.jwt.token.expire_length.access_token}") final long accessTokenValidityInSeconds,
+            @Value("${security.jwt.token.expire_length.refresh_token}") final long refreshTokenValidityInSeconds) {
+        this.inMemoryRefreshTokenRepository = inMemoryRefreshTokenRepository;
         this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
         this.accessTokenValidityInSeconds = accessTokenValidityInSeconds;
         this.refreshTokenValidityInSeconds = refreshTokenValidityInSeconds;
@@ -47,6 +51,11 @@ public class JwtTokenProvider {
     }
 
     public String createRefreshToken(final long memberId) {
+        if(!inMemoryRefreshTokenRepository.existsById(memberId)) {
+            String newRefreshToken = createToken(String.valueOf(memberId), refreshTokenValidityInSeconds);
+            inMemoryRefreshTokenRepository.save(memberId, newRefreshToken);
+            return newRefreshToken;
+        }
         return createToken(String.valueOf(memberId), refreshTokenValidityInSeconds);
     }
 
