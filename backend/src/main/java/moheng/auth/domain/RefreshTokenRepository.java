662417cea929cf728eea6a218e686a838df4b3c5
Haon <88240193+msung99@users.noreply.github.com>
2024-07-29 13:14:28 +0900

    #35 feat: 테스트 환경에서 환경 초기화 및 공통 설정을 위한 계층별 Config 생성

--- backend/src/main/java/moheng/auth/domain/RefreshTokenRepository.java
+++ backend/src/main/java/moheng/auth/domain/RefreshTokenRepository.java
@@ -4,4 +4,5 @@ public interface RefreshTokenRepository {
     void save(final long memberId, final String refreshToken);
     boolean existsById(final long memberId);
     String findById(final long memberId);
+    void deleteAll();
 }
