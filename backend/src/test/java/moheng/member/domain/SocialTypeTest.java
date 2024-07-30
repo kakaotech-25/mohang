10faaea9f6dba20da5a2a24878b901db8561dcea
Haon <88240193+msung99@users.noreply.github.com>
2024-07-30 16:48:01 +0900

    #47 test: 구글 소셜 계정 타입을 정상적으로 찾는지 테스트

--- backend/src/test/java/moheng/member/domain/SocialTypeTest.java
+++ backend/src/test/java/moheng/member/domain/SocialTypeTest.java
@@ -13,8 +13,15 @@ public class SocialTypeTest {
 
     @DisplayName("카카오 소셜 계정 타입을 찾는다.")
     @Test
-    public void 카카오_소셜_계정_타입을_찾는다() {
+    void 카카오_소셜_계정_타입을_찾는다() {
         assertEquals(SocialType.KAKAO, SocialType.findByName("KAKAO"));
-        assertEquals(SocialType.GOOGLE, SocialType.findByName("kakao"));
+        assertEquals(SocialType.KAKAO, SocialType.findByName("kakao"));
+    }
+
+    @DisplayName("구글 소셜 계정 타입을 찾는다.")
+    @Test
+    void 구글_소셜_계정_타입을_찾는다() {
+        assertEquals(SocialType.GOOGLE, SocialType.findByName("GOOGLE"));
+        assertEquals(SocialType.GOOGLE, SocialType.findByName("google"));
     }
 }
