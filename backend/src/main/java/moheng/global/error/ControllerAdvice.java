1992948f68394ace34fe70a3ba2ce96d729645a2
Haon <88240193+msung99@users.noreply.github.com>
2024-07-30 15:37:57 +0900

    #47 feat: ControllerAdivce 내에 예외 발생시 응답 DTO 정의 및 Exception 처리

--- backend/src/main/java/moheng/global/error/ControllerAdvice.java
+++ backend/src/main/java/moheng/global/error/ControllerAdvice.java
@@ -1,23 +1,22 @@
 package moheng.global.error;
 
-import moheng.global.error.dto.ErrorResponse;
+import moheng.global.error.dto.ExceptionResponse;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @RestControllerAdvice
 public class ControllerAdvice {
     private static final Logger logger = LoggerFactory.getLogger(ControllerAdvice.class);
 
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleRunTimeException(Exception e) {
-        ErrorResponse errorResponse = new ErrorResponse();
-        return ResponseEntity.badRequest().body(errorResponse);
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleOverflowException(final Exception e) {
+        logger.error(e.getMessage(), e);
+
+        return ResponseEntity.internalServerError()
+                .body(new ExceptionResponse("서버에 예기치 못한 오류가 발생했습니다. 관리자에게 문의하세요."));
     }
 }
