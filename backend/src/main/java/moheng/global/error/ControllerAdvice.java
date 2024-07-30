7b6ff02f970927535141eccc30a2951e3f577f06
Haon <88240193+msung99@users.noreply.github.com>
2024-07-30 16:02:55 +0900

    #47 feat: 잘못된 형식의 Request Body 에 대한 예외처리

--- backend/src/main/java/moheng/global/error/ControllerAdvice.java
+++ backend/src/main/java/moheng/global/error/ControllerAdvice.java
@@ -7,6 +7,7 @@ import org.apache.coyote.Response;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -53,13 +54,10 @@ public class ControllerAdvice {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
     }
 
-
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ExceptionResponse> handleRuntimeException(final RuntimeException e) {
-        logger.error(e.getMessage(), e);
-
-        return ResponseEntity.badRequest()
-                .body(new ExceptionResponse(e.getMessage()));
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ExceptionResponse> handleInvalidRequestBody() {
+        ExceptionResponse exceptionResponse = new ExceptionResponse("잘못된 Request Body 형식 입니다.");
+        return ResponseEntity.badRequest().body(exceptionResponse);
     }
 
     @ExceptionHandler(Exception.class)
