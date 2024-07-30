715711464eb52756cddeae580643b5ace0924ce2
Haon <88240193+msung99@users.noreply.github.com>
2024-07-30 17:09:05 +0900

    #47 chore: given-when-then 누락된 구문 추가

--- backend/src/test/java/moheng/member/domain/SocialTypeTest.java
+++ backend/src/test/java/moheng/member/domain/SocialTypeTest.java
@@ -14,6 +14,7 @@ public class SocialTypeTest {
     @DisplayName("카카오 소셜 계정 타입을 찾는다.")
     @Test
     void 카카오_소셜_계정_타입을_찾는다() {
+        // given, when, then
         assertEquals(SocialType.KAKAO, SocialType.findByName("KAKAO"));
         assertEquals(SocialType.KAKAO, SocialType.findByName("kakao"));
     }
@@ -21,6 +22,7 @@ public class SocialTypeTest {
     @DisplayName("구글 소셜 계정 타입을 찾는다.")
     @Test
     void 구글_소셜_계정_타입을_찾는다() {
+        // given, when, then
         assertEquals(SocialType.GOOGLE, SocialType.findByName("GOOGLE"));
         assertEquals(SocialType.GOOGLE, SocialType.findByName("google"));
     }
@@ -39,6 +41,7 @@ public class SocialTypeTest {
     @DisplayName("일치하는 소셜 타입이 있다면 참을 리턴한다.")
     @Test
     void 일치하는_소셜_타입이_있다면_거짓을_리턴한다() {
+        // given, when, then
         assertThat(SocialType.isMatches(SocialType.KAKAO)).isTrue();
         assertThat(SocialType.isMatches(SocialType.GOOGLE)).isTrue();
 
@@ -47,6 +50,7 @@ public class SocialTypeTest {
     @DisplayName("일치하는 소셜 타입이 없다면 거짓을 리턴한다.")
     @Test
     void 일치하는_소셜_타입이_없다면_거짓을_리턴한다() {
+        // given, when, then
         assertThat(SocialType.isMatches(null)).isFalse();
     }
 }
