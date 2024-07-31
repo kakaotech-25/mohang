3cd2e9515ddf49a88658f4338d691c2ec5d1cc59
Haon <88240193+msung99@users.noreply.github.com>
2024-07-31 16:39:47 +0900

    #55 test: 리프레시 토큰을 통해 새로운 엑세스 토큰을 발급하면 상태코드 200을 리턴하는지 테스트

--- backend/src/test/java/moheng/auth/presentation/AuthControllerTest.java
+++ backend/src/test/java/moheng/auth/presentation/AuthControllerTest.java
@@ -1,7 +1,6 @@
 package moheng.auth.presentation;
 
-import static moheng.fixture.AuthFixtures.토큰_생성_요청;
-import static moheng.fixture.AuthFixtures.토큰_응답;
+import static moheng.fixture.AuthFixtures.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
@@ -19,6 +18,7 @@ import static org.springframework.restdocs.request.RequestDocumentation.pathPara
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import moheng.auth.domain.token.MemberToken;
 import moheng.auth.dto.RenewalAccessTokenResponse;
 import moheng.auth.dto.TokenRequest;
@@ -42,9 +42,9 @@ public class AuthControllerTest extends ControllerTestConfig {
                 .andExpect(status().isOk());
     }
 
-    @DisplayName("OAuth 로그인을 하면 토큰과 상태코드 200을 리턴한다.")
+    @DisplayName("OAuth 로그인을 하면 토큰과 상태코드 201을 리턴한다.")
     @Test
-    public void OAuth_로그인을_하면_토큰과_상태코드_200을_반환한다() throws Exception {
+    public void OAuth_로그인을_하면_토큰과_상태코드_201을_반환한다() throws Exception {
         // given
         given(authService.generateTokenWithCode(any(), any())).willReturn(토큰_응답());
 
@@ -55,5 +55,19 @@ public class AuthControllerTest extends ControllerTestConfig {
                         .content(objectMapper.writeValueAsString(토큰_생성_요청())))
                 .andDo(print())
                 .andExpect(status().isCreated());
+    }
+
+    @DisplayName("리프레시 토큰을 통해 새로운 엑세스 토큰을 발급하면 상태코드 200을 리턴한다.")
+    @Test
+    void 리프레시_토큰을_통해_새로운_엑세스_토큰을_발급하면_상태코드_200을_리턴한다() throws Exception{
+        // given
+        given(authService.generateRenewalAccessToken(any())).willReturn(토큰_갱신_응답());
+
+        // when, then
+        mockMvc.perform(post("/auth/extend/login")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .cookie(토큰_갱신_요청())
+        ).andDo(print()).andExpect(status().isCreated());
     }
 }
--- backend/src/test/java/moheng/fixture/AuthFixtures.java
+++ backend/src/test/java/moheng/fixture/AuthFixtures.java
@@ -1,6 +1,9 @@
 package moheng.fixture;
 
+import jakarta.servlet.http.Cookie;
 import moheng.auth.domain.token.MemberToken;
+import moheng.auth.dto.RenewalAccessTokenRequest;
+import moheng.auth.dto.RenewalAccessTokenResponse;
 import moheng.auth.dto.TokenRequest;
 
 public class AuthFixtures {
@@ -14,5 +17,13 @@ public class AuthFixtures {
 
     public static MemberToken 토큰_응답() {
         return new MemberToken("access-token", "refresh-token");
+    }
+
+    public static Cookie 토큰_갱신_요청() {
+        return new Cookie("refresh-token", "token-value");
+    }
+
+    public static RenewalAccessTokenResponse 토큰_갱신_응답() {
+        return new RenewalAccessTokenResponse("access-token");
     }
 }
