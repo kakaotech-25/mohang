1eef9ba693f20a00c7c59e9d2a9f3e06b44ed4dc
Haon <88240193+msung99@users.noreply.github.com>
2024-07-31 15:34:59 +0900

    #55 test: 소셜 로그인을 위한 링크와 상태코드 200을 리턴하는지 테스트

--- backend/src/test/java/moheng/auth/presentation/AuthControllerTest.java
+++ backend/src/test/java/moheng/auth/presentation/AuthControllerTest.java
@@ -1,4 +1,37 @@
 package moheng.auth.presentation;
 
-public class AuthControllerTest {
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import moheng.config.ControllerTestConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.restdocs.payload.JsonFieldType;
+
+public class AuthControllerTest extends ControllerTestConfig {
+    @DisplayName("소셜 로그인을 위한 링크와 상태코드 200을 반환한다.")
+    @Test
+    public void OAuth_소셜_로그인을_위한_링크와_상태코드_200을_반환한다() throws Exception {
+        // given
+        given(authService.generateUri(anyString())).willReturn("URI");
+
+        // when, then
+        mockMvc.perform(get("/auth/{oAuthProvider}/link", "KAKAO"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
 }
---
+++ backend/src/test/java/moheng/config/ControllerTestConfig.java
@@ -0,0 +1,27 @@
+package moheng.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import moheng.auth.application.AuthService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public abstract class ControllerTestConfig {
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @MockBean
+    protected AuthService authService;
+}
