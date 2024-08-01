44c7f8abdcf1c9a0b5467fc0b84b10804728de12
Haon <88240193+msung99@users.noreply.github.com>
2024-08-01 12:13:05 +0900

    #56 test: 최초 사용자나 기존에 존재하는 회원이 다시 로그인하는 경우 토큰을 발급하고 상태코드 201을 리턴하는지 인수 테스트

--- backend/src/test/java/moheng/acceptance/AuthAcceptanceTest.java
+++ backend/src/test/java/moheng/acceptance/AuthAcceptanceTest.java
@@ -9,7 +9,10 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import moheng.auth.dto.AccessTokenResponse;
 import moheng.auth.dto.OAuthUriResponse;
+import moheng.auth.dto.TokenRequest;
+import moheng.auth.dto.TokenResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
@@ -42,5 +45,34 @@ public class AuthAcceptanceTest extends AcceptanceTestConfig {
 
     public static void 상태코드_200이_반환된다(final ExtractableResponse<Response> response) {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+
+    @DisplayName("최초 사용자나 기존에 존재하는 회원이 다시 로그인하는 경우 토큰을 발급하고 상태코드 201을 리턴한다.")
+    @Test
+    void 최초_사용자나_기존에_존재하는_회원이_다시_로그인하는_경우_토큰을_발급하고_상태코드_200을_리턴한다() {
+        // given, when
+        ExtractableResponse<Response> response = 자체_토큰을_생성한다("KAKAO", "authorization-code");
+        AccessTokenResponse tokenResponse = response.as(AccessTokenResponse.class);
+
+        // then
+        assertAll(() -> {
+            상태코드_201이_반환된다(response);
+            assertThat(tokenResponse.getAccessToken()).isNotEmpty();
+        });
+    }
+
+    public static ExtractableResponse<Response> 자체_토큰을_생성한다(final String oauthProvider, final String code) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(new TokenRequest(code))
+                .when().post("/auth/{oAuthProvider}/login", oauthProvider)
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value())
+                .extract();
+    }
+
+    public static void 상태코드_201이_반환된다(final ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 }
