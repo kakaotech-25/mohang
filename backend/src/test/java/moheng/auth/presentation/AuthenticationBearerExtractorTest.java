542d11b38c29a7ded9a64ce750c5e3e71ed18526
Haon <88240193+msung99@users.noreply.github.com>
2024-08-05 16:22:13 +0900

    #68 test: 헤더의 Authorization 토큰 값이 유효하지 않다면 예외가 발생하는지 테스트

--- backend/src/test/java/moheng/auth/presentation/AuthenticationBearerExtractorTest.java
+++ backend/src/test/java/moheng/auth/presentation/AuthenticationBearerExtractorTest.java
@@ -7,6 +7,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 
 import moheng.auth.exception.EmptyBearerHeaderException;
+import moheng.auth.exception.InvalidTokenException;
 import moheng.auth.exception.InvalidTokenFormatException;
 import moheng.auth.presentation.authentication.AuthenticationBearerExtractor;
 import moheng.config.ControllerTestConfig;
@@ -22,5 +23,14 @@ public class AuthenticationBearerExtractorTest extends ControllerTestConfig {
 
         assertThatThrownBy(() -> authenticationBearerExtractor.extract(httpServletRequest))
                 .isInstanceOf(EmptyBearerHeaderException.class);
+    }
+
+    @DisplayName("헤더의 Authorization 값이 유효하지 않다면 예외가 발생한다.")
+    @Test
+    void 헤더의_Authorization_값이_유효하지_않다면_예외가_발생한다() {
+        given(httpServletRequest.getHeader("Authorization")).willReturn("invalid token");
+
+        assertThatThrownBy(() -> authenticationBearerExtractor.extract(httpServletRequest))
+                .isInstanceOf(InvalidTokenFormatException.class);
     }
 }
