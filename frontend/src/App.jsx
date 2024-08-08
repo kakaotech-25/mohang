c1ebbc71feecec54396abb9f1647e89758f89210
LEE-DA-EUN <delee3064@gmail.com>
2024-08-08 16:27:49 +0900

    #89 add : 페이지라우팅

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,31 @@
 import './App.css'
 import {Routes, Route} from "react-router-dom"
+import Header from "./components/Header.jsx"
+import Footer from "./components/Footer.jsx"
+import Home from "./pages/Home.jsx"
 import Login from './pages/Login.jsx'
+import Profile from './pages/Profile.jsx'
+import Mypage from './pages/Mypage.jsx'
+import Place from './pages/Place.jsx'
+import Planner from './pages/Planner.jsx'
+import PlannerList from './pages/PlannerList.jsx'
 
 function App() {
 
   return (
     <>
-    <h1>test</h1>
-    <Routes>
-      <Route path="/login" element={<Login />} />
-    </Routes>
+      
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/mypage" element={<Mypage />} />
+        <Route path="/place" element={<Place />} />
+        <Route path="/planner" element={<Planner />} />
+        <Route path="/plannerlist" element={<PlannerList />} />
+      </Routes>
+      <Footer />
     </>
   )
 }
