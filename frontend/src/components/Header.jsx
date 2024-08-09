aa34bbfbbf440e8353f4dbdf2327442efd0b6a6d
LEE-DA-EUN <delee3064@gmail.com>
2024-08-09 18:23:26 +0900

    #89 add: 로그인 전 헤더 구성

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,10 +4,10 @@ import "./Header.css"
 const Header = () => {
   return (
     <section className='header'>
-      <section className='logo'>
-        <img src={logo} className='logoimg' />
-        <div>모행</div>
-      </section>
+      <img src={logo} className='logoimg' />
+      <div className='moheng'>모행</div>
+
+      <div className='login'>로그인</div>
 
     </section>
   );
