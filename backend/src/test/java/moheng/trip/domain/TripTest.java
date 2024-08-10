7130709c96bed57e390042d875bf4afc05cec5de
msung99 <msung6924@naver.com>
2024-08-10 22:20:26 +0900

    #103 test: 여행지를 정상 생성하는지 테스트

--- backend/src/test/java/moheng/trip/domain/TripTest.java
+++ backend/src/test/java/moheng/trip/domain/TripTest.java
@@ -9,6 +9,15 @@ import org.junit.jupiter.api.Test;
 
 public class TripTest {
 
+    @DisplayName("여행지를 생성한다.")
+    @Test
+    void 여행지를_생성한다() {
+        // given, when, then
+        assertDoesNotThrow(() -> new Trip("롯데월드2", "서울특별시 송파구2", 1000000L,
+                "서울 롯데월드는 신나는 여가와 엔터테인먼트를 꿈꾸는 사람들과 갈수록 늘어나는 외국인 관광 활성화를 위해 운영하는 테마파크예요.2",
+                "https://lotte-world-image2.png"));
+    }
+
     @DisplayName("여행지 이름의 길이가 유효하지 않다면 예외가 발생한다.")
     @Test
     void 여행지_이름의_길이가_유효하지_않다면_예외가_발생한다() {
