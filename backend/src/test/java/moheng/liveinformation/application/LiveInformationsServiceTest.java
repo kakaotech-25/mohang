42396043fe17f228388bca7d3763b727ada6b82b
msung99 <msung6924@naver.com>
2024-08-10 13:00:46 +0900

    #103 test: 생활정보를 저장하는지 테스트

--- backend/src/test/java/moheng/liveinformation/application/LiveInformationsServiceTest.java
+++ backend/src/test/java/moheng/liveinformation/application/LiveInformationsServiceTest.java
@@ -15,6 +15,13 @@ public class LiveInformationsServiceTest extends ServiceTestConfig {
     @Autowired
     private LiveInformationService liveInformationService;
 
+    @DisplayName("생활정보를 저장한다.")
+    @Test
+    void 생활정보를_저장한다() {
+        // given, when, then
+        liveInformationService.save(new LiveInformation("생활정보 이름"));
+    }
+
     @DisplayName("생활정보 이름으로 생활정보를 찾는다.")
     @Test
     void 생활정보_이름으로_생활정보를_찾는다() {
