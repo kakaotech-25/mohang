ef36b8a74fb8f7a373c04411a3b42eb47f61d8e1
msung99 <msung6924@naver.com>
2024-08-10 21:55:46 +0900

    #103 test: 여행지 정보를 저장하는지 테스트

--- backend/src/test/java/moheng/trip/application/TripServiceTest.java
+++ backend/src/test/java/moheng/trip/application/TripServiceTest.java
@@ -14,6 +14,15 @@ public class TripServiceTest extends ServiceTestConfig {
     @Autowired
     private TripService tripService;
 
+    @DisplayName("여행지 정보를 저장한다.")
+    @Test
+    void 여행지_정보를_저장한다() {
+        // given
+        tripService.save(new Trip("롯데월드2", "서울특별시 송파구2", 1000000L,
+                "서울 롯데월드는 신나는 여가와 엔터테인먼트를 꿈꾸는 사람들과 갈수록 늘어나는 외국인 관광 활성화를 위해 운영하는 테마파크예요.2",
+                "https://lotte-world-image2.png"));
+    }
+
     @DisplayName("여행지 정보를 가져온다.")
     @Test
     void 여행지_정보를_가져온다() {
