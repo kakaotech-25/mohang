42ff890fb909b0f6246487b30379019453257f42
msung99 <msung6924@naver.com>
2024-08-11 22:33:12 +0900

    #104 feat: 유저의 생활정보 조회 컨트롤러 구현

--- backend/src/main/java/moheng/liveinformation/presentation/LiveInformationController.java
+++ backend/src/main/java/moheng/liveinformation/presentation/LiveInformationController.java
@@ -1,8 +1,12 @@
 package moheng.liveinformation.presentation;
 
+import moheng.auth.dto.Accessor;
+import moheng.auth.presentation.authentication.Authentication;
 import moheng.liveinformation.application.LiveInformationService;
+import moheng.liveinformation.application.MemberLiveInformationService;
 import moheng.liveinformation.domain.LiveInformation;
 import moheng.liveinformation.dto.FindAllLiveInformationResponse;
+import moheng.liveinformation.dto.FindMemberLiveInformationResponses;
 import moheng.liveinformation.dto.LiveInformationCreateRequest;
 import moheng.liveinformation.dto.LiveInformationResponse;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +18,15 @@ import java.util.List;
 @RestController
 public class LiveInformationController {
     private final LiveInformationService liveInformationService;
+    private final MemberLiveInformationService memberLiveInformationService;
 
-    public LiveInformationController(final LiveInformationService liveInformationService) {
+    public LiveInformationController(final LiveInformationService liveInformationService,
+                                     final MemberLiveInformationService memberLiveInformationService) {
         this.liveInformationService = liveInformationService;
+        this.memberLiveInformationService = memberLiveInformationService;
     }
 
-    @GetMapping
+    @GetMapping("/all")
     public ResponseEntity<FindAllLiveInformationResponse> findAllLiveInformation() {
         return ResponseEntity.ok(liveInformationService.findAllLiveInformation());
     }
@@ -28,5 +35,10 @@ public class LiveInformationController {
     public ResponseEntity<Void> createLiveInformation(final @RequestBody LiveInformationCreateRequest request) {
         liveInformationService.createLiveInformation(request);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/member")
+    public ResponseEntity<FindMemberLiveInformationResponses> findAllLiveInformationByUser(@Authentication Accessor accessor) {
+        return ResponseEntity.ok(memberLiveInformationService.findMemberSelectedLiveInformation(accessor.getId()));
     }
 }
