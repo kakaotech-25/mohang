935455bb5e12e8d528c8ce51717f00f7f83528fa
msung99 <msung6924@naver.com>
2024-08-11 23:23:28 +0900

    #104 test: 생활정보 이름의 길이가 올바르지 않다면 예외가 발생하는지 테스트

--- backend/src/test/java/moheng/liveinformation/domain/LiveInformationTest.java
+++ backend/src/test/java/moheng/liveinformation/domain/LiveInformationTest.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import moheng.liveinformation.exception.LiveInfoNameException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class LiveInformationTest {
     @DisplayName("생활정보를 생성한다.")
@@ -17,9 +19,10 @@ public class LiveInformationTest {
     }
 
     @DisplayName("생활정보 이름의 길이가 올바르지 않다면 예외가 발생한다.")
-    @Test
-    void 생활정보_이름의_길이가_올바르지_않다면_예외가_발생한다() {
+    @ParameterizedTest
+    @ValueSource(strings = {"", "일이삼자오육칠팔구십 일이삼자오육칠팔구십 일이삼자오육칠팔구십 일이삼자오육칠팔구십 일이삼자오육칠팔구십 일이삼자오육칠팔구십 일이삼자오육칠팔구십 일이삼자오육칠팔구십 일이삼자오육칠팔구십 일이삼자오육칠팔구십 "})
+    void 생활정보_이름의_길이가_올바르지_않다면_예외가_발생한다(final String name) {
         // given, when, then
-        assertThatThrownBy(() -> new LiveInformation("")).isInstanceOf(LiveInfoNameException.class);
+        assertThatThrownBy(() -> new LiveInformation(name)).isInstanceOf(LiveInfoNameException.class);
     }
 }
