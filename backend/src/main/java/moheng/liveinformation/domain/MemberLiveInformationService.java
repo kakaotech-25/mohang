38e6f6614feef64a0677e389972e7c85216121ea
msung99 <msung6924@naver.com>
2024-08-11 22:18:44 +0900

    #104 feat: 유저가 선택한 생활정보, 선택하지 않은 생활정보 조회 기능 구현

--- backend/src/main/java/moheng/liveinformation/domain/MemberLiveInformationService.java
+++ backend/src/main/java/moheng/liveinformation/domain/MemberLiveInformationService.java
@@ -1,18 +1,47 @@
 package moheng.liveinformation.domain;
 
+import moheng.liveinformation.dto.FindAllLiveInformationResponse;
+import moheng.liveinformation.dto.FindMemberLiveInformationResponses;
+import moheng.liveinformation.dto.LiveInfoResponse;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class MemberLiveInformationService {
     private final MemberLiveInformationRepository memberLiveInformationRepository;
+    private final LiveInformationRepository liveInformationRepository;
 
-    public MemberLiveInformationService(MemberLiveInformationRepository memberLiveInformationRepository) {
+    public MemberLiveInformationService(
+            final MemberLiveInformationRepository memberLiveInformationRepository,
+            final LiveInformationRepository liveInformationRepository) {
         this.memberLiveInformationRepository = memberLiveInformationRepository;
+        this.liveInformationRepository = liveInformationRepository;
     }
 
     public void saveAll(List<MemberLiveInformation> memberLiveInformations) {
         memberLiveInformationRepository.saveAll(memberLiveInformations);
+    }
+
+    public FindMemberLiveInformationResponses findMemberSelectedLiveInformation(Long memberId) {
+        final List<LiveInformation> allLiveInformation = liveInformationRepository.findAll();
+        final List<Long> memberLiveInfoIds = findMemberLiveInfoIds(memberId);
+        return new FindMemberLiveInformationResponses(findMemberSelectedLiveInfos(allLiveInformation, memberLiveInfoIds));
+    }
+
+    private List<Long> findMemberLiveInfoIds(final Long memberId) {
+        return memberLiveInformationRepository.findByMemberId(memberId)
+                .stream().map(memberLiveInfo -> memberLiveInfo.getLiveInformation().getId())
+                .collect(Collectors.toList());
+    }
+
+    private List<LiveInfoResponse> findMemberSelectedLiveInfos(final List<LiveInformation> allLiveInformation, List<Long> selectedLiveInfoIds) {
+        return allLiveInformation.stream()
+                .map(liveInfo -> new LiveInfoResponse(
+                        liveInfo.getId(),
+                        liveInfo.getName(),
+                        selectedLiveInfoIds.contains(liveInfo.getId())))
+                .collect(Collectors.toList());
     }
 }
