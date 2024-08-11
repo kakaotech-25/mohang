72c0811ed40f0c77e62e3421299c280f48aa4c00
msung99 <msung6924@naver.com>
2024-08-11 13:45:17 +0900

    #103 refactor: 여행지 생성 로직 픽스쳐로 분리

--- backend/src/test/java/moheng/acceptance/TripAcceptenceTest.java
+++ backend/src/test/java/moheng/acceptance/TripAcceptenceTest.java
@@ -1,5 +1,6 @@
 package moheng.acceptance;
 
+import static moheng.acceptance.fixture.TripAcceptenceFixture.*;
 import static moheng.acceptance.fixture.HttpStatus.상태코드_200이_반환된다;
 import static moheng.acceptance.fixture.HttpStatus.상태코드_204이_반환된다;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -24,13 +25,7 @@ public class TripAcceptenceTest extends AcceptanceTestConfig {
     @Test
     void 여행지를_생성하면_상태코드_204를_리턴한다() {
         // given, when
-        ExtractableResponse<Response> resultResponse = RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(new TripCreateRequest("롯데월드", "서울특별시 송파구", 1L, "설명", "https://lotte-world.png"))
-                .when().post("/trip")
-                .then().log().all()
-                .statusCode(HttpStatus.NO_CONTENT.value())
-                .extract();
+        ExtractableResponse<Response> resultResponse = 여행지를_생성한다("롯데월드", 1L);
 
         // then
         assertAll(() -> {
