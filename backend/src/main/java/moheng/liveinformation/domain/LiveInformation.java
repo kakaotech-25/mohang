683fa5d488c1c8324036650fc83ea2677dcd4c38
msung99 <msung6924@naver.com>
2024-08-11 23:17:40 +0900

    #104 test: 생활정보를 생성 테스트

--- backend/src/main/java/moheng/liveinformation/domain/LiveInformation.java
+++ backend/src/main/java/moheng/liveinformation/domain/LiveInformation.java
@@ -2,10 +2,14 @@ package moheng.liveinformation.domain;
 
 import jakarta.persistence.*;
 import moheng.global.entity.BaseEntity;
+import moheng.liveinformation.exception.LiveInfoNameException;
 
 @Table(name = "live_information")
 @Entity
 public class LiveInformation extends BaseEntity {
+    private static final int MIN_NAME_LENGTH = 1;
+    private static final int MAX_NAME_LENGTH = 100;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
@@ -18,12 +22,20 @@ public class LiveInformation extends BaseEntity {
     }
 
     public LiveInformation(Long id, String name) {
+        validateName(name);
         this.id = id;
         this.name = name;
     }
 
     public LiveInformation(String name) {
+        validateName(name);
         this.name = name;
+    }
+
+    private void validateName(String name) {
+        if(name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH) {
+            throw new LiveInfoNameException("생활정보 이름의 길이는 최소 1자 이상, 최대 100자 이하만 허용됩니다.");
+        }
     }
 
     public Long getId() {
