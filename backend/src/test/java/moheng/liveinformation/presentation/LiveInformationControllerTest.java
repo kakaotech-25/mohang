a401ec641557c4b7bb76ea5862f2ea640f968d14
msung99 <msung6924@naver.com>
2024-08-11 14:16:23 +0900

    #103 test: 모든 생활정보를 찾고 상태코드 200을 리턴하는지 테스트

--- backend/src/test/java/moheng/liveinformation/presentation/LiveInformationControllerTest.java
+++ backend/src/test/java/moheng/liveinformation/presentation/LiveInformationControllerTest.java
@@ -1,18 +1,40 @@
 package moheng.liveinformation.presentation;
 
-import moheng.config.ControllerTestConfig;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static moheng.fixture.MemberFixtures.비어있는_생활정보로_회원가입_요청;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
+import moheng.config.ControllerTestConfig;
+import moheng.liveinformation.domain.LiveInformation;
+import moheng.liveinformation.dto.FindAllLiveInformationResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import java.util.List;
+
 public class LiveInformationControllerTest extends ControllerTestConfig {
+
+    @DisplayName("모든 생활정보를 찾고 상태코드 200을 리턴한다.")
+    @Test
+    void 모든_생활정보를_찾고_상태코드_200을_리턴한다() throws Exception {
+        // given
+        given(liveInformationService.findAllLiveInformation())
+                .willReturn(new FindAllLiveInformationResponse(
+                        List.of(new LiveInformation("생활정보1"), new LiveInformation("생활정보2"), new LiveInformation("생활정보3"))));
+
+        // when, then
+        mockMvc.perform(get("/live/info")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andDo(print())
+                .andExpect(status().isOk());
+    }
 
     @DisplayName("생활정보를 생성하면 상태코드 204를 리턴한다.")
     @Test
