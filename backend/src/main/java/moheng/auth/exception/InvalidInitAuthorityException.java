7a9fd26b65ab658803c29bb7817e3ceb7f6ba041
msung99 <msung6924@naver.com>
2024-08-12 15:22:12 +0900

    #112 feat: 초기 회원가입 기능에 대한 권한이 없을 때 예외가 발생하는 기능 구현

---
+++ backend/src/main/java/moheng/auth/exception/InvalidInitAuthorityException.java
@@ -0,0 +1,7 @@
+package moheng.auth.exception;
+
+public class InvalidInitAuthorityException extends RuntimeException {
+    public InvalidInitAuthorityException(String message) {
+        super(message);
+    }
+}
--- backend/src/main/java/moheng/auth/presentation/initauthentication/InitAuthenticationArgumentResolver.java
+++ backend/src/main/java/moheng/auth/presentation/initauthentication/InitAuthenticationArgumentResolver.java
@@ -1,11 +1,16 @@
 package moheng.auth.presentation.initauthentication;
 
 import jakarta.servlet.http.HttpServletRequest;
+import moheng.auth.domain.oauth.Authority;
 import moheng.auth.domain.token.JwtTokenProvider;
 import moheng.auth.dto.Accessor;
 import moheng.auth.exception.BadRequestException;
+import moheng.auth.exception.InvalidInitAuthorityException;
 import moheng.auth.presentation.authentication.Authentication;
 import moheng.auth.presentation.authentication.AuthenticationBearerExtractor;
+import moheng.member.domain.Member;
+import moheng.member.domain.repository.MemberRepository;
+import moheng.member.exception.NoExistMemberException;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -17,10 +22,14 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 public class InitAuthenticationArgumentResolver implements HandlerMethodArgumentResolver {
     private final JwtTokenProvider jwtTokenProvider;
     private final AuthenticationBearerExtractor authenticationBearerExtractor;
+    private final MemberRepository memberRepository;
 
-    public InitAuthenticationArgumentResolver(final JwtTokenProvider jwtTokenProvider, final AuthenticationBearerExtractor authenticationBearerExtractor) {
+    public InitAuthenticationArgumentResolver(final JwtTokenProvider jwtTokenProvider,
+                                              final AuthenticationBearerExtractor authenticationBearerExtractor,
+                                              final MemberRepository memberRepository) {
         this.jwtTokenProvider = jwtTokenProvider;
         this.authenticationBearerExtractor = authenticationBearerExtractor;
+        this.memberRepository = memberRepository;
     }
 
     @Override
@@ -45,6 +54,12 @@ public class InitAuthenticationArgumentResolver implements HandlerMethodArgument
         jwtTokenProvider.validateToken(accessToken);
         final Long id = jwtTokenProvider.getMemberId(accessToken);
 
+        final Member initMember = memberRepository.findById(id)
+                .orElseThrow(() -> new NoExistMemberException("존재하지 않는 유저입니다."));
+
+        if(!initMember.getAuthority().equals(Authority.INIT_MEMBER)) {
+            throw new InvalidInitAuthorityException("초기 회원가입 기능에 대한 접근 권한이 없습니다.");
+        }
         return new Accessor(id);
     }
 }
