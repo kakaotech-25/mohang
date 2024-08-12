6147e053cc41c685d1677f9eebef805e42052caf
msung99 <msung6924@naver.com>
2024-08-12 10:52:09 +0900

    #104 test: 여행지 엔티티에 대한 단위 테스트 추가

--- backend/src/test/java/moheng/trip/domain/TripTest.java
+++ backend/src/test/java/moheng/trip/domain/TripTest.java
@@ -1,12 +1,17 @@
 package moheng.trip.domain;
 
+import static moheng.fixture.MemberFixtures.하온_소셜_타입_구글;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
+import moheng.member.domain.Member;
+import moheng.member.exception.InvalidEmailFormatException;
 import moheng.trip.exception.InvalidTripDescriptionException;
 import moheng.trip.exception.InvalidTripNameException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class TripTest {
 
@@ -20,10 +25,9 @@ public class TripTest {
     }
 
     @DisplayName("여행지 이름의 길이가 유효하지 않다면 예외가 발생한다.")
-    @Test
-    void 여행지_이름의_길이가_유효하지_않다면_예외가_발생한다() {
-        String name = "";
-
+    @ValueSource(strings = {"", "a", "여", "행", "일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 "})
+    @ParameterizedTest
+    void 여행지_이름의_길이가_유효하지_않다면_예외가_발생한다(final String name) {
         assertThatThrownBy(() -> new Trip(name, "서울특별시 송파구", 1000000L,
                 "서울 롯데월드는 신나는 여가와 엔터테인먼트를 꿈꾸는 사람들과 갈수록 늘어나는 외국인 관광 활성화를 위해 운영하는 테마파크예요.",
                 "https://lotte-world-image.png"))
@@ -31,11 +35,10 @@ public class TripTest {
     }
 
     @DisplayName("여행지 장소명의 길이가 유효하지 않다면 예외가 발생한다.")
-    @Test
-    void 여행지_장소명의_길이가_유효하지_않다면_예외가_발생한다() {
+    @ValueSource(strings = {"", "a", "여", "행", "일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 "})
+    @ParameterizedTest
+    void 여행지_장소명의_길이가_유효하지_않다면_예외가_발생한다(final String placeName) {
         // given, when, then
-        String placeName = "";
-
         assertThatThrownBy(() -> new Trip("롯데월드", placeName, 1000000L,
                 "서울 롯데월드는 신나는 여가와 엔터테인먼트를 꿈꾸는 사람들과 갈수록 늘어나는 외국인 관광 활성화를 위해 운영하는 테마파크예요.",
                 "https://lotte-world-image.png"))
@@ -43,16 +46,13 @@ public class TripTest {
     }
 
     @DisplayName("여행지에 대한 설명이 유효하지 않다면 예외가 발생한다")
-    @Test
-    void 여행지에_대한_설명이_유효하지_않다면_예외가_발생한다() {
+    @ValueSource(strings = {"", "a", "여", "행"})
+    @ParameterizedTest
+    void 여행지에_대한_설명이_유효하지_않다면_예외가_발생한다(final String description) {
         // given, when, then
-        String descprtion = "";
-
         assertThatThrownBy(() -> new Trip("롯데월드", "서울특별시 송파구", 1000000L,
-                descprtion,
+                description,
                 "https://lotte-world-image.png"))
                 .isInstanceOf(InvalidTripDescriptionException.class);
     }
-
-
 }
