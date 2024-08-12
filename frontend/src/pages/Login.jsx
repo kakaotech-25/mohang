5d3625f17c3d2e392255dfd92f99956b6b5cf41f
LEE-DA-EUN <delee3064@gmail.com>
2024-08-12 16:36:22 +0900

    #115 add: 로그인 화면 사진 추가

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,31 @@
+import "./Login.css"
+import Left1 from '../assets/login-left-1.png'
+import Left2 from '../assets/login-left-2.png'
+import Left3 from '../assets/login-left-3.png'
+import Right1 from '../assets/login-right-1.png'
+import Right2 from '../assets/login-right-2.png'
+import Right3 from '../assets/login-right-3.png'
+
+
 const Login = () => {
-    return <div>Login</div>;
+    return (
+      <section className="loginpage">
+        <div className="login_left">
+          <img src={Left3} className='left3img' />
+          <img src={Left2} className='left2img' />
+          <img src={Left1} className='left1img' />
+        </div>
+        <div className="kakaologin">
+
+        </div>
+        <div className="login_right">
+          <img src={Right1} className='right1img' />
+          <img src={Right2} className='right2img' />
+          <img src={Right3} className='right3img' />
+        </div>
+
+      </section>
+    );
   };
   
   export default Login;
