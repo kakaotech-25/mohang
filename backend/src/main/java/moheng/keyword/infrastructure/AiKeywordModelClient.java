db57351a9689e2382878acc7bf9e71d229757d16
msung99 <msung6924@naver.com>
2024-08-13 13:26:14 +0900

    #107 feat: AI 와 키워드 기반 추천 여행지 응답을 받아오는 기능 구현

--- backend/src/main/java/moheng/keyword/infrastructure/AiKeywordModelClient.java
+++ backend/src/main/java/moheng/keyword/infrastructure/AiKeywordModelClient.java
@@ -1,14 +1,21 @@
 package moheng.keyword.infrastructure;
 
+import moheng.auth.domain.oauth.OAuthAccessToken;
 import moheng.keyword.dto.TripContentIdsByKeywordResponse;
 import moheng.keyword.exception.TripRecommendByKeywordRequest;
 import moheng.keyword.service.KeywordFilterModelClient;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Optional;
 
 @Component
 public class AiKeywordModelClient implements KeywordFilterModelClient {
-    private static final String KEYWORD_LIST_REQUEST_URL = "http://localhost:8000";
+    private static final String RECOMMEND_TRIP_LIST_BY_KEYWORD_REQUEST_URL = "http://localhost:8000";
     private final RestTemplate restTemplate;
 
     public AiKeywordModelClient(final RestTemplate restTemplate) {
@@ -17,6 +24,12 @@ public class AiKeywordModelClient implements KeywordFilterModelClient {
 
     @Override
     public TripContentIdsByKeywordResponse findRecommendTripContentIdsByKeywords(TripRecommendByKeywordRequest request) {
-        return null;
+        return requestRecommendTrips(request);
+    }
+
+    private TripContentIdsByKeywordResponse requestRecommendTrips(TripRecommendByKeywordRequest request) {
+        final ResponseEntity<TripContentIdsByKeywordResponse> contentIds = restTemplate.postForEntity(RECOMMEND_TRIP_LIST_BY_KEYWORD_REQUEST_URL, request, TripContentIdsByKeywordResponse.class);
+        return Optional.ofNullable(contentIds.getBody())
+                .orElseThrow(IllegalArgumentException::new);
     }
 }
