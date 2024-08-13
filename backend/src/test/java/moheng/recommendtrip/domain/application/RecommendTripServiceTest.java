60ed34a3e18776ad3b37542b67cfe6a2432c33d9
msung99 <msung6924@naver.com>
2024-08-13 19:43:07 +0900

    #107 test: 추천 여행지 정보를 저장하는지 테스트

--- backend/src/test/java/moheng/recommendtrip/domain/application/RecommendTripServiceTest.java
+++ backend/src/test/java/moheng/recommendtrip/domain/application/RecommendTripServiceTest.java
@@ -11,6 +11,7 @@ import moheng.member.application.MemberService;
 import moheng.member.domain.Member;
 import moheng.member.domain.repository.MemberRepository;
 import moheng.recommendtrip.application.RecommendTripService;
+import moheng.recommendtrip.dto.RecommendTripCreateRequest;
 import moheng.trip.application.TripService;
 import moheng.trip.domain.Trip;
 import moheng.trip.repository.TripRepository;
@@ -37,5 +38,16 @@ public class RecommendTripServiceTest extends ServiceTestConfig {
 
         // when, then
         assertDoesNotThrow(() -> recommendTripService.saveByRank(trip, member, 1L));
+    }
+
+    @DisplayName("추천 여행지 정보를 저장한다.")
+    @Test
+    void 추천_여행지_정보를_저장한다() {
+        // given
+        Member member = memberRepository.save(하온_기존());
+        Trip trip = tripRepository.save(new Trip("여행지", "장소명", 1L, "설명", "이미지 경로"));
+
+        // when, then
+        assertDoesNotThrow(() -> recommendTripService.createRecommendTrip(member.getId(), new RecommendTripCreateRequest(trip.getId())));
     }
 }
