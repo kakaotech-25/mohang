b779a7ec37f1860eab0b942193c394911a0d58e6
LEE-DA-EUN <delee3064@gmail.com>
2024-08-13 15:35:19 +0900

    #119 feat: 최초입력창 상단설명 컴포넌트 구현

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,5 +1,12 @@
+import ProfileInfo from "../components/ProfileInfo";
+import "./Profile.css"
+
 const Profile = () => {
-    return <div>Profile</div>;
+    return (
+      <section className="profilepage">
+        <ProfileInfo text={"회원가입을 위해 프로필 정보를 입력해주세요."} />
+      </section>
+    );
   };
   
   export default Profile;
