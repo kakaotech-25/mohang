01f87650007603d39e9c7e80c96e6c7a601516ba
LEE-DA-EUN <delee3064@gmail.com>
2024-08-13 16:50:27 +0900

    #119 feat: 프로필 사진 추가

---
+++ frontend/src/components/ProgressBar.jsx
@@ -0,0 +1,11 @@
+import "./ProgressBar.css"
+
+const ProgressBar = ({total, now}) => {
+    return (
+        <section className="progressbar">
+            progressbar 자리
+        </section>
+    );
+};
+
+export default ProgressBar;
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,12 +1,26 @@
 import ProfileInfo from "../components/ProfileInfo";
+import ProgressBar from "../components/ProgressBar";
 import "./Profile.css"
+import profileimg1 from "../assets/profileimg1.png"
+import profileimg2 from "../assets/profileimg2.png"
+import profileimg3 from "../assets/profileimg3.png"
 
 const Profile = () => {
-    return (
-      <section className="profilepage">
-        <ProfileInfo text={"회원가입을 위해 프로필 정보를 입력해주세요."} />
+  return (
+    <section className="profilepage">
+      <ProfileInfo text={"회원가입을 위해 프로필 정보를 입력해주세요."} />
+      <ProgressBar />
+      <section className='profile1'>
+        <section className="profileimgs">
+          <img src={profileimg1} className='profleimg1' />
+          <img src={profileimg2} className='profleimg2' />
+          <img src={profileimg3} className='profleimg3' />
+        </section>
+
       </section>
-    );
-  };
-  
-  export default Profile;
+
+    </section>
+  );
+};
+
+export default Profile;
