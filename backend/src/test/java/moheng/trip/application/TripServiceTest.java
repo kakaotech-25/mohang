f1a910b7d772234a9d0aacc7fda8045d9abd0897
msung99 <msung6924@naver.com>
2024-08-15 11:48:41 +0900

    #105 test: 기존에 rank가 1등인 선호 여행지가 존재하지 않으면 예외가 발생하는지 테스트

--- backend/src/test/java/moheng/trip/application/TripServiceTest.java
+++ backend/src/test/java/moheng/trip/application/TripServiceTest.java
@@ -16,6 +16,7 @@ import moheng.recommendtrip.domain.RecommendTripRepository;
 import moheng.trip.domain.Trip;
 import moheng.trip.dto.FindTripWithSimilarTripsResponse;
 import moheng.trip.dto.TripCreateRequest;
+import moheng.trip.exception.NoExistRecommendTripException;
 import moheng.trip.exception.NoExistTripException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -431,5 +432,41 @@ public class TripServiceTest extends ServiceTestConfig {
             assertThat(recommendTripRepository.existsByMemberAndTrip(member, trip1)).isFalse();
             assertThat(recommendTripRepository.findById(2L).get().getRank()).isEqualTo(HIGHEST_PRIORITY_RANK);
         });
+    }
+
+
+    @DisplayName("기존에 rank가 1등인 선호 여행지가 존재하지 않으면 예외가 발생한다.")
+    @Test
+    void 기존에_rank가_1등인_선호_여행지가_존재하지_않으면_예외가_발생한다() {
+        // given
+        Member member = memberRepository.save(하온_기존());
+        tripService.save(new Trip("여행지1", "서울", 1L, "설명1", "https://image.png", 0L));
+        tripService.save(new Trip("여행지2", "서울", 2L, "설명2", "https://image.png", 0L));
+        tripService.save(new Trip("여행지3", "서울", 3L, "설명3", "https://image.png", 0L));
+        tripService.save(new Trip("여행지4", "서울", 4L, "설명4", "https://image.png", 0L));
+        tripService.save(new Trip("여행지5", "서울", 5L, "설명5", "https://image.png", 0L));
+        tripService.save(new Trip("여행지6", "서울", 6L, "설명6", "https://image.png", 0L));
+        tripService.save(new Trip("여행지7", "서울", 7L, "설명7", "https://image.png", 0L));
+        tripService.save(new Trip("여행지8", "서울", 8L, "설명8", "https://image.png", 0L));
+        tripService.save(new Trip("여행지9", "서울", 9L, "설명9", "https://image.png", 0L));
+        tripService.save(new Trip("여행지10", "서울", 10L, "설명10", "https://image.png", 0L));
+        Trip trip1 = tripService.findById(1L); Trip trip2 = tripService.findById(2L);
+        Trip trip3 = tripService.findById(3L); Trip trip4 = tripService.findById(4L);
+        Trip trip5 = tripService.findById(5L); Trip trip6 = tripService.findById(6L);
+        Trip trip7 = tripService.findById(7L); Trip trip8 = tripService.findById(8L);
+        Trip trip9 = tripService.findById(9L); Trip trip10 = tripService.findById(10L);
+
+        recommendTripRepository.save(new RecommendTrip(trip1, member, 0L)); recommendTripRepository.save(new RecommendTrip(trip2, member, 2L));
+        recommendTripRepository.save(new RecommendTrip(trip3, member, 3L)); recommendTripRepository.save(new RecommendTrip(trip4, member, 4L));
+        recommendTripRepository.save(new RecommendTrip(trip5, member, 5L)); recommendTripRepository.save(new RecommendTrip(trip6, member, 6L));
+        recommendTripRepository.save(new RecommendTrip(trip7, member, 7L)); recommendTripRepository.save(new RecommendTrip(trip8, member, 8L));
+        recommendTripRepository.save(new RecommendTrip(trip9, member, 9L)); recommendTripRepository.save(new RecommendTrip(trip10, member, 10L));
+
+        // when
+        Trip trip = tripService.findById(10L);
+
+        // then
+        assertThatThrownBy(() -> tripService.findWithSimilarOtherTrips(trip.getId(), member.getId()))
+                .isInstanceOf(NoExistRecommendTripException.class);
     }
 }
