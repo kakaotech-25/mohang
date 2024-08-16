2298ee2a6c160f41e05a8cfd234bf31cd582059a
LEE-DA-EUN <delee3064@gmail.com>
2024-08-16 13:51:13 +0900

    #128 feat: 상단 설명내용 추가

--- frontend/src/pages/InterestedPlace/InterestedPlace.jsx
+++ frontend/src/pages/InterestedPlace/InterestedPlace.jsx
@@ -1,7 +1,12 @@
 import "./InterestedPlace.css"
+import ProfileInfo from "../../components/ProfileInfo/ProfileInfo";
 
 const InterestedPlace = () => {
-    return <div>Profile3</div>;
+    return (
+        <section className="interested-page">
+            <ProfileInfo text={"AI 맞춤 추천을 위해 관심 여행지를 5개 이상 선택해주세요."} />
+        </section>
+    );
 };
 
 export default InterestedPlace;
