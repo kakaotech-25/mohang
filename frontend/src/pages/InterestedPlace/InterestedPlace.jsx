f6263f117f1a5eccccc984acd17f6ee8eb23afd2
LEE-DA-EUN <delee3064@gmail.com>
2024-08-16 13:54:54 +0900

    #128 feat: 관심여행지 progressbar 추가

--- frontend/src/pages/InterestedPlace/InterestedPlace.jsx
+++ frontend/src/pages/InterestedPlace/InterestedPlace.jsx
@@ -1,10 +1,14 @@
 import "./InterestedPlace.css"
 import ProfileInfo from "../../components/ProfileInfo/ProfileInfo";
+import progressbar3 from "../../assets/progressbar3.png"
 
 const InterestedPlace = () => {
     return (
         <section className="interested-page">
             <ProfileInfo text={"AI 맞춤 추천을 위해 관심 여행지를 5개 이상 선택해주세요."} />
+            <section className="interested-progressbar">
+                <img src={progressbar3} />
+            </section>
         </section>
     );
 };
