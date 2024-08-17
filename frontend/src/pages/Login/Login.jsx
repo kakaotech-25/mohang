b504166af628278c1bdab2b230863a7c71ee9614
do-yoon <do.yoon.like.cheeze@gmail.com>
2024-08-17 14:20:08 +0900

    #139 test: OAuth 로그인 링크 생성 API를 호출하는 코드 작성

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -2,15 +2,34 @@ import "./Login.css"
 import Left from '../../assets/login-left.png'
 import Right from '../../assets/login-right.png'
 import KakaoBtn from '../../assets/kakao-login.png'
+import axios from 'axios'
+import {useEffect, useState} from "react";
 
-
+// axios 적용을 위해 비동기 함수 선언
 const Login = () => {
+    const [kakaoLink, setKakaoLink] = useState("")
+
+    // baseurl을 따로 설정해주는 것을 추천합니다.
+    // https://blog.beomi.net/api-url-feat-react-and-axios
+    useEffect(() => {
+        async function fetchKakaoLink() {
+            const link = await axios.get("http://localhost:8080/auth/KAKAO/link")
+                .then((response) => response.data["oAuthUri"])
+            setKakaoLink(link);
+        }
+
+        if (!kakaoLink) {
+            fetchKakaoLink();
+        }
+    }, [])
+
     return (
       <section className="login-page">
         <div className="login-left">
           <img src={Left}/>
         </div>
         <div className="kakao-login">
+          <a style={{width: "100px", height: "100px"}} href={kakaoLink}/>
           <img src={KakaoBtn} />
         </div>
         <div className="login-right">
@@ -19,6 +38,6 @@ const Login = () => {
 
       </section>
     );
-  };
+};
   
-  export default Login;
+export default Login;
