4bfc5287e19f65869f093573b29325601f9a81e2
msung99 <msung6924@naver.com>
2024-08-18 19:31:39 +0900

    #108 test: AI 맞춤 10개의 여행지를 추천받는지 테스트

--- backend/src/test/java/moheng/config/stub/StubRecommendTripModelClient.java
+++ backend/src/test/java/moheng/config/stub/StubRecommendTripModelClient.java
@@ -7,7 +7,7 @@ import moheng.trip.dto.RecommendTripsByVisitedLogsResponse;
 import java.util.List;
 
 public class StubRecommendTripModelClient implements ExternalRecommendModelClient {
-    private static final List<Long> contentIds = List.of(1L, 2L, 3L);
+    private static final List<Long> contentIds = List.of(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L);
 
     @Override
     public RecommendTripsByVisitedLogsResponse recommendTripsByVisitedLogs(RecommendTripsByVisitedLogsRequest request) {
