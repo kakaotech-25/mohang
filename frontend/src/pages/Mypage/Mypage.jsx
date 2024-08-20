91f4ba9700a212aa17c3e7077bdea8956dbdf414
LEE-DA-EUN <delee3064@gmail.com>
2024-08-20 16:53:45 +0900

    #161 feat: 프로필/생활정보를 한페이지에서 탭으로 확인하도록 탭 기능 구현

--- frontend/src/pages/Mypage/Mypage.jsx
+++ frontend/src/pages/Mypage/Mypage.jsx
@@ -1,5 +1,44 @@
-const Mypage = () => {
-    return <div>Mypage</div>;
+import { useState } from 'react';
+import './Mypage.css';
+
+function Mypage() {
+  const [activeTab, setActiveTab] = useState('tab1');
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
   };
-  
-  export default Mypage;
+
+  return (
+    <div className="my-page">
+      <section className="tabs">
+        <button
+          className={activeTab === 'tab1' ? 'tab-button active' : 'tab-button'}
+          onClick={() => handleTabClick('tab1')}
+        >
+          프로필
+        </button>
+        <button
+          className={activeTab === 'tab2' ? 'tab-button active' : 'tab-button'}
+          onClick={() => handleTabClick('tab2')}
+        >
+          생활
+        </button>
+      </section>
+
+      <section className="tab-content">
+        {activeTab === 'tab1' && (
+          <div id="tab1">
+            <h1>프로필 공간</h1>
+          </div>
+        )}
+        {activeTab === 'tab2' && (
+          <div id="tab2">
+            <h1>생활정보 공간</h1>
+          </div>
+        )}
+      </section>
+    </div>
+  );
+}
+
+export default Mypage;
