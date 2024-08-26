f99cd34243cc1e65f88d28f1cb269ceb3f2c91e3
msung99 <msung6924@naver.com>
2024-08-26 13:10:46 +0900

    #148 test: 플래너 일정의 이름이 유효하지 않다면 예외가 발생하는지 테스트

--- backend/src/test/java/moheng/planner/domain/PlanTest.java
+++ backend/src/test/java/moheng/planner/domain/PlanTest.java
@@ -6,8 +6,12 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import moheng.member.domain.Member;
+import moheng.member.exception.InvalidEmailFormatException;
+import moheng.planner.exception.InvalidPlanNameException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.LocalDate;
 
@@ -20,5 +24,15 @@ public class PlanTest {
                 LocalDate.of(2024, 8, 26),
                 LocalDate.of(2024, 8, 27),
                 하온_기존()));
+    }
+
+    @DisplayName("플래너 일정의 이름이 유효하지 않다면 예외가 발생한다.")
+    @ValueSource(strings = {"", "1", "플", "랜",
+            "일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 일이삼사오육칠팔구십 "})
+    @ParameterizedTest
+    void 플래너_일정의_이름이_유효하지_않다면_예외가_발생한다(final String planName) {
+        assertThatThrownBy(() ->
+                new Plan(planName, LocalDate.of(2024, 8, 26), LocalDate.of(2024, 8, 27), 하온_기존())
+        ).isInstanceOf(InvalidPlanNameException.class);
     }
 }
