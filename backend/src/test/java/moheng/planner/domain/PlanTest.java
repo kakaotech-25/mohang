4387c9ebf3362779cc2c6b64e31f5e212451407b
msung99 <msung6924@naver.com>
2024-08-26 13:16:11 +0900

    #148 test: 플래너 일정의 시작날짜가 종료날짜의 이후라면 예외가 발생하는지 테스트

--- backend/src/test/java/moheng/planner/domain/PlanTest.java
+++ backend/src/test/java/moheng/planner/domain/PlanTest.java
@@ -7,6 +7,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import moheng.member.domain.Member;
 import moheng.member.exception.InvalidEmailFormatException;
+import moheng.planner.exception.InvalidPlanDateException;
 import moheng.planner.exception.InvalidPlanNameException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -34,5 +35,13 @@ public class PlanTest {
         assertThatThrownBy(() ->
                 new Plan(planName, LocalDate.of(2024, 8, 26), LocalDate.of(2024, 8, 27), 하온_기존())
         ).isInstanceOf(InvalidPlanNameException.class);
+    }
+
+    @DisplayName("플래너 일정의 시작날짜가 종료날짜의 이후라면 예외가 발생한다.")
+    @Test
+    void 플래너_일정의_시작날짜가_종료날짜의_이후라면_예외가_발생한다() {
+        assertThatThrownBy(() ->
+            new Plan("일정", LocalDate.of(2024, 8, 26), LocalDate.of(1000, 1, 1), 하온_기존())
+        ).isInstanceOf(InvalidPlanDateException.class);
     }
 }
