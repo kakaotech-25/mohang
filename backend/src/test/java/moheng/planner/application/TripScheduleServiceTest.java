7409ee19ad3acc6195e04afafb063513a84f09cc
msung99 <msung6924@naver.com>
2024-08-26 13:46:09 +0900

    #148 test: 존재하지 않는 멤버의 여행 일정을 생성하면 예외가 발생하는지 테스트

--- backend/src/test/java/moheng/planner/application/TripScheduleServiceTest.java
+++ backend/src/test/java/moheng/planner/application/TripScheduleServiceTest.java
@@ -8,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import moheng.config.slice.ServiceTestConfig;
 import moheng.member.domain.Member;
 import moheng.member.domain.repository.MemberRepository;
+import moheng.member.exception.NoExistMemberException;
 import moheng.planner.dto.CreateTripScheduleRequest;
 import moheng.planner.exception.AlreadyExistTripScheduleException;
 import org.junit.jupiter.api.DisplayName;
@@ -51,5 +52,18 @@ public class TripScheduleServiceTest extends ServiceTestConfig {
             tripScheduleService.createTripSchedule(member.getId(),
                     new CreateTripScheduleRequest("기존일정", LocalDate.of(2024, 6, 1), LocalDate.of(2024, 6, 2))
         )).isInstanceOf(AlreadyExistTripScheduleException.class);
+    }
+
+    @DisplayName("존재하지 않는 멤버의 여행 일정을 생성하면 예외가 발생한다.")
+    @Test
+    void 존재하지_않는_멤버의_여행_일정을_생성하면_예외가_발생한다() {
+        // given
+        long invalidMemberId = -1L;
+
+        // when, then
+        assertThatThrownBy(() ->
+                tripScheduleService.createTripSchedule(invalidMemberId,
+                        new CreateTripScheduleRequest("신규일정", LocalDate.of(2024, 6, 1), LocalDate.of(2024, 6, 2))
+                )).isInstanceOf(NoExistMemberException.class);
     }
 }
