46b904b60886291506e0702cd54ea660b3df9122
msung99 <msung6924@naver.com>
2024-08-26 13:44:31 +0900

    #148 test: 동일한 이름을 가진 멤버의 여행 일정이 이미 존재하면 예외가 발생하는지 테스트

--- backend/src/test/java/moheng/planner/application/TripScheduleServiceTest.java
+++ backend/src/test/java/moheng/planner/application/TripScheduleServiceTest.java
@@ -9,6 +9,7 @@ import moheng.config.slice.ServiceTestConfig;
 import moheng.member.domain.Member;
 import moheng.member.domain.repository.MemberRepository;
 import moheng.planner.dto.CreateTripScheduleRequest;
+import moheng.planner.exception.AlreadyExistTripScheduleException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,5 +36,20 @@ public class TripScheduleServiceTest extends ServiceTestConfig {
                     new CreateTripScheduleRequest("일정1", LocalDate.of(2024, 8, 1), LocalDate.of(2024, 8, 2))
             );
         });
+    }
+
+    @DisplayName("동일한 이름을 가진 멤버의 여행 일정이 이미 존재하면 예외가 발생한다.")
+    @Test
+    void 동일한_이름을_가진_멤버의_여행_일정이_이미_존재하면_예외가_발생한다() {
+        // given
+        Member member = memberRepository.save(하온_기존());
+        tripScheduleService.createTripSchedule(member.getId(),
+                new CreateTripScheduleRequest("기존일정", LocalDate.of(2024, 8, 1), LocalDate.of(2024, 8, 2)));
+
+        // when, then
+        assertThatThrownBy(() ->
+            tripScheduleService.createTripSchedule(member.getId(),
+                    new CreateTripScheduleRequest("기존일정", LocalDate.of(2024, 6, 1), LocalDate.of(2024, 6, 2))
+        )).isInstanceOf(AlreadyExistTripScheduleException.class);
     }
 }
