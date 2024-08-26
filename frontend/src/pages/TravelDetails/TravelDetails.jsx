a4b4af731fb9493783700c002d765ea2c2ef1d23
Daeun Lee <56716976+LEE-DA-EUN@users.noreply.github.com>
2024-08-26 17:24:07 +0900

    #158 test: 여행지카드별 세부페이지 정보가 잘 들어가는지 테스트

--- frontend/src/pages/TravelDetails/TravelDetails.jsx
+++ frontend/src/pages/TravelDetails/TravelDetails.jsx
@@ -1,8 +1,23 @@
 import './TravelDetails.css'
-
+import { useParams } from 'react-router-dom';
+import TravelData from '../../data/TravelData';
 
 const TravelDetails = () => {
-    return <div>test</div>;
+  const { id } = useParams();
+  const card = TravelData.find(item => item.id === parseInt(id));
+
+  return (
+    <div>
+      <h1>{card.title}</h1>
+      <img src={card.image} alt={card.title} />
+      <p>위치: {card.location}</p>
+      <div>
+        {card.tags.map((tag, index) => (
+          <span key={index}>#{tag} </span>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default TravelDetails;
