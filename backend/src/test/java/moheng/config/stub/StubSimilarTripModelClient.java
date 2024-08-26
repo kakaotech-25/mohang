30dfbeceb9a2f0344eb6ea678adaba21590f3f5c
msung99 <msung6924@naver.com>
2024-08-26 10:05:49 +0900

    #154 feat: 비슷한 여행지 추천 AI 클라이언트 응답 필드 변경

--- backend/src/test/java/moheng/config/stub/StubSimilarTripModelClient.java
+++ backend/src/test/java/moheng/config/stub/StubSimilarTripModelClient.java
@@ -6,7 +6,7 @@ import moheng.trip.dto.FindSimilarTripWithContentIdResponses;
 import java.util.List;
 
 public class StubSimilarTripModelClient implements ExternalSimilarTripModelClient {
-    private static final List<Long> contentIds = List.of(1L, 2L, 3L);
+    private static final List<Long> contentIds = List.of(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L, 11L, 12L);
 
     @Override
     public FindSimilarTripWithContentIdResponses findSimilarTrips(long contentId) {
