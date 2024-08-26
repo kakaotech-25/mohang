089504a38abf047ec776d98fb8590f9d4c1ba5a1
msung99 <msung6924@naver.com>
2024-08-26 18:24:40 +0900

    #148 test: 플래너의 여행지를 이름순으로 조회하고 상태코드 200을 리턴하는지 테스트

--- backend/src/test/java/moheng/planner/presentation/PlannerControllerTest.java
+++ backend/src/test/java/moheng/planner/presentation/PlannerControllerTest.java
@@ -19,6 +19,7 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 
 import moheng.config.slice.ControllerTestConfig;
 import moheng.planner.domain.TripSchedule;
+import moheng.planner.dto.FindPLannerOrderByNameResponse;
 import moheng.planner.dto.FindPlannerOrderByRecentResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -31,7 +32,7 @@ public class PlannerControllerTest extends ControllerTestConfig {
 
     @DisplayName("플래너의 여행지를 최신순으로 조회하고 상태코드 200을 리턴한다.")
     @Test
-    void 플래너의_여행지를_최신순으로_조회학도_상태코드_200을_리턴한다() throws Exception {
+    void 플래너의_여행지를_최신순으로_조회하고_상태코드_200을_리턴한다() throws Exception {
         // given
         given(jwtTokenProvider.getMemberId(anyString())).willReturn(1L);
         given(plannerService.findPlannerOrderByRecent(anyLong())).willReturn(
@@ -49,6 +50,36 @@ public class PlannerControllerTest extends ControllerTestConfig {
                 .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andDo(document("planner/find/recent",
+                        preprocessRequest(),
+                        responseFields(
+                                fieldWithPath("tripScheduleResponses").description("여행 일정 리스트 : 최신순조회"),
+                                fieldWithPath("tripScheduleResponses[].scheduleName").description("여행 일정 이름"),
+                                fieldWithPath("tripScheduleResponses[].startTime").description("여행 일정 시작날짜"),
+                                fieldWithPath("tripScheduleResponses[].endTime").description("여행 일정 종료날짜")
+                        )))
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("플래너의 여행지를 이름순으로 조회하고 상태코드 200을 리턴한다.")
+    @Test
+    void 플래너의_여행지를_이름순으로_조회하고_상태코드_200을_리턴한다() throws Exception {
+        // given
+        given(jwtTokenProvider.getMemberId(anyString())).willReturn(1L);
+        given(plannerService.findPlannerOrderByName(anyLong())).willReturn(
+                new FindPLannerOrderByNameResponse(List.of(
+                        new TripSchedule("가 여행", LocalDate.of(2024, 3, 10), LocalDate.of(2030, 1, 10), 하온_기존()),
+                        new TripSchedule("나 여행", LocalDate.of(2023, 2, 15), LocalDate.of(2030, 1, 10), 하온_기존()),
+                        new TripSchedule("다 여행", LocalDate.of(2022, 1, 8), LocalDate.of(2030, 1, 10), 하온_기존()),
+                        new TripSchedule("라 여행", LocalDate.of(2021, 1, 1), LocalDate.of(2030, 1, 10), 하온_기존())
+                ))
+        );
+
+        // when, then
+        mockMvc.perform(get("/api/planner/name")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andDo(document("planner/find/name",
                         preprocessRequest(),
                         responseFields(
                                 fieldWithPath("tripScheduleResponses").description("여행 일정 리스트 : 최신순조회"),
