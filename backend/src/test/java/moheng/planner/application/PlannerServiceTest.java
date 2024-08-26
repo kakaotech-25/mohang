3902841071b1b185790eca9425e6f8866637f78f
msung99 <msung6924@naver.com>
2024-08-26 16:36:39 +0900

    #148 test: 플래너 여행 일정을 이름순으로 오름차순 정렬하는지 테스트

--- backend/src/test/java/moheng/planner/application/PlannerServiceTest.java
+++ backend/src/test/java/moheng/planner/application/PlannerServiceTest.java
@@ -10,6 +10,7 @@ import moheng.member.domain.Member;
 import moheng.member.domain.repository.MemberRepository;
 import moheng.planner.domain.TripSchedule;
 import moheng.planner.domain.TripScheduleRepository;
+import moheng.planner.dto.FindPLannerOrderByNameResponse;
 import moheng.planner.dto.FindPlannerOrderByDateResponse;
 import moheng.planner.dto.FindPlannerOrderByRecentResponse;
 import moheng.planner.dto.TripScheduleResponse;
@@ -70,6 +71,28 @@ public class PlannerServiceTest extends ServiceTestConfig {
         assertAll(() -> {
             assertThat(tripScheduleResponses.get(0).getScheduleName()).isEqualTo("일정1");
             assertThat(tripScheduleResponses.get(3).getScheduleName()).isEqualTo("일정4");
+            assertThat(tripScheduleResponses.size()).isEqualTo(4);
+        });
+    }
+
+    @DisplayName("플래너 여행 일정을 이름순으로 오름차순 정렬한다.")
+    @Test
+    void 플래너_여행_일정을_이름순으로_오름차순_정렬한다() {
+        // given
+        Member member = memberRepository.save(하온_기존());
+        tripScheduleRepository.save(new TripSchedule("가 일정", LocalDate.of(2020, 8, 1), LocalDate.of(2024, 8, 2), member));
+        tripScheduleRepository.save(new TripSchedule("나 일정", LocalDate.of(2020, 8, 1), LocalDate.of(2024, 8, 2), member));
+        tripScheduleRepository.save(new TripSchedule("다 일정", LocalDate.of(2020, 8, 1), LocalDate.of(2024, 8, 2), member));
+        tripScheduleRepository.save(new TripSchedule("라 일정", LocalDate.of(2020, 8, 1), LocalDate.of(2024, 8, 2), member));
+
+        // when
+        FindPLannerOrderByNameResponse response = plannerService.findPlannerOrderByName(member.getId());
+        List<TripScheduleResponse> tripScheduleResponses = response.getTripScheduleResponses();
+
+        // then
+        assertAll(() -> {
+            assertThat(tripScheduleResponses.get(0).getScheduleName()).isEqualTo("가 일정");
+            assertThat(tripScheduleResponses.get(3).getScheduleName()).isEqualTo("라 일정");
             assertThat(tripScheduleResponses.size()).isEqualTo(4);
         });
     }
