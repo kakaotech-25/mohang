86a11597ad617ad4359241cf361bf04d9546b140
msung99 <msung6924@naver.com>
2024-08-26 10:58:36 +0900

    #154 test: 처음 방문한 여행지라면 방문 횟수는 1이 되는지 재테스트

--- backend/src/test/java/moheng/trip/application/TripServiceTest.java
+++ backend/src/test/java/moheng/trip/application/TripServiceTest.java
@@ -625,35 +625,42 @@ public class TripServiceTest extends ServiceTestConfig {
     void 처음_방문한_여행지라면_방문_횟수는_1이_된다() {
         // given
         Member member = memberRepository.save(하온_기존());
-        tripService.save(new Trip("여행지1", "서울", 1L, "설명1", "https://image.png", 0L));
-        tripService.save(new Trip("여행지2", "서울", 2L, "설명2", "https://image.png", 0L));
-        tripService.save(new Trip("여행지3", "서울", 3L, "설명3", "https://image.png", 0L));
-        tripService.save(new Trip("여행지4", "서울", 4L, "설명4", "https://image.png", 0L));
-        tripService.save(new Trip("여행지5", "서울", 5L, "설명5", "https://image.png", 0L));
-        tripService.save(new Trip("여행지6", "서울", 6L, "설명6", "https://image.png", 0L));
-        tripService.save(new Trip("여행지7", "서울", 7L, "설명7", "https://image.png", 0L));
-        tripService.save(new Trip("여행지8", "서울", 8L, "설명8", "https://image.png", 0L));
-        tripService.save(new Trip("여행지9", "서울", 9L, "설명9", "https://image.png", 0L));
-        tripService.save(new Trip("여행지10", "서울", 10L, "설명10", "https://image.png", 0L));
-        tripService.save(new Trip("여행지11", "서울", 11L, "설명10", "https://image.png", 0L));
-        Trip trip1 = tripService.findById(1L); Trip trip2 = tripService.findById(2L);
-        Trip trip3 = tripService.findById(3L); Trip trip4 = tripService.findById(4L);
-        Trip trip5 = tripService.findById(5L); Trip trip6 = tripService.findById(6L);
-        Trip trip7 = tripService.findById(7L); Trip trip8 = tripService.findById(8L);
-        Trip trip9 = tripService.findById(9L); Trip trip10 = tripService.findById(10L);
-        Trip firstVisitTrip = tripService.findById(11L);
+        Trip currentTrip = tripRepository.save(new Trip("여행지1", "서울", 1L, "설명1", "https://image.png", 0L));
+        Trip trip2 = tripRepository.save(new Trip("여행지2", "서울", 2L, "설명2", "https://image.png", 0L));
+        Trip trip3 = tripRepository.save(new Trip("여행지3", "서울", 3L, "설명3", "https://image.png", 0L));
+        Trip trip4 = tripRepository.save(new Trip("여행지4", "서울", 4L, "설명4", "https://image.png", 0L));
+        Trip trip5 = tripRepository.save(new Trip("여행지5", "서울", 5L, "설명5", "https://image.png", 0L));
+        Trip trip6 = tripRepository.save(new Trip("여행지6", "서울", 6L, "설명6", "https://image.png", 0L));
+        Trip trip7 = tripRepository.save(new Trip("여행지7", "서울", 7L, "설명7", "https://image.png", 0L));
+        Trip trip8 = tripRepository.save(new Trip("여행지8", "서울", 8L, "설명8", "https://image.png", 0L));
+        Trip trip9 = tripRepository.save(new Trip("여행지9", "서울", 9L, "설명9", "https://image.png", 0L));
+        Trip trip10 = tripRepository.save(new Trip("여행지10", "서울", 10L, "설명10", "https://image.png", 0L));
+        Trip trip11 = tripRepository.save(new Trip("여행지11", "서울", 11L, "설명10", "https://image.png", 0L));
 
-        recommendTripRepository.save(new RecommendTrip(trip1, member, 1L)); recommendTripRepository.save(new RecommendTrip(trip2, member, 2L));
-        recommendTripRepository.save(new RecommendTrip(trip3, member, 3L)); recommendTripRepository.save(new RecommendTrip(trip4, member, 4L));
-        recommendTripRepository.save(new RecommendTrip(trip5, member, 5L)); recommendTripRepository.save(new RecommendTrip(trip6, member, 6L));
-        recommendTripRepository.save(new RecommendTrip(trip7, member, 7L)); recommendTripRepository.save(new RecommendTrip(trip8, member, 8L));
-        recommendTripRepository.save(new RecommendTrip(trip9, member, 9L)); recommendTripRepository.save(new RecommendTrip(trip10, member, 10L));
+        LiveInformation liveInformation = liveInformationRepository.save(new LiveInformation("생활정보1"));
+        tripLiveInformationRepository.save(new TripLiveInformation(liveInformation, currentTrip));
+        tripLiveInformationRepository.save(new TripLiveInformation(liveInformation, trip2)); tripLiveInformationRepository.save(new TripLiveInformation(liveInformation, trip3));
+        tripLiveInformationRepository.save(new TripLiveInformation(liveInformation, trip4)); tripLiveInformationRepository.save(new TripLiveInformation(liveInformation, trip5));
+        tripLiveInformationRepository.save(new TripLiveInformation(liveInformation, trip6)); tripLiveInformationRepository.save(new TripLiveInformation(liveInformation, trip7));
+        tripLiveInformationRepository.save(new TripLiveInformation(liveInformation, trip8)); tripLiveInformationRepository.save(new TripLiveInformation(liveInformation, trip9));
+        tripLiveInformationRepository.save(new TripLiveInformation(liveInformation, trip10)); tripLiveInformationRepository.save(new TripLiveInformation(liveInformation, trip11));
+
+        recommendTripRepository.save(new RecommendTrip(trip2, member, 1L));
+        recommendTripRepository.save(new RecommendTrip(trip3, member, 2L));
+        recommendTripRepository.save(new RecommendTrip(trip4, member, 3L));
+        recommendTripRepository.save(new RecommendTrip(trip5, member, 4L));
+        recommendTripRepository.save(new RecommendTrip(trip6, member, 5L));
+        recommendTripRepository.save(new RecommendTrip(trip7, member, 6L));
+        recommendTripRepository.save(new RecommendTrip(trip8, member, 7L));
+        recommendTripRepository.save(new RecommendTrip(trip9, member, 8L));
+        recommendTripRepository.save(new RecommendTrip(trip10, member, 9L));
+        recommendTripRepository.save(new RecommendTrip(trip11, member, 10L));
 
         // when
-        tripService.findWithSimilarOtherTrips(firstVisitTrip.getId(), member.getId());
+        tripService.findWithSimilarOtherTrips(currentTrip.getId(), member.getId());
 
         // then
-        assertThat(memberTripRepository.findByMemberAndTrip(member, firstVisitTrip).getVisitedCount()).isEqualTo(1L);
+        assertThat(memberTripRepository.findByMemberAndTrip(member, currentTrip).getVisitedCount()).isEqualTo(1L);
     }
 
     @DisplayName("이전에 방문했던 여행지라면 방문 횟수가 1이 증가한다.")
