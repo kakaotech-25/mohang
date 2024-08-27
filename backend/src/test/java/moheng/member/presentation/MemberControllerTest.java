6a9a037d6d75123668614f706255478d90e12ed2
msung99 <msung6924@naver.com>
2024-08-27 23:00:25 +0900

    #178 test: 프로필 정보로 회원가입시 입력한 성별이 유효하지 않다면 상태코드 400을 리턴하는지 테스트

--- backend/src/test/java/moheng/member/presentation/MemberControllerTest.java
+++ backend/src/test/java/moheng/member/presentation/MemberControllerTest.java
@@ -169,7 +169,40 @@ public class MemberControllerTest extends ControllerTestConfig {
                         .content(objectMapper.writeValueAsString(유효하지_않은_닉네임_프로필_정보로_회원가입_요청()))
                 )
                 .andDo(print())
-                .andDo(document("member/signup/profile/fail/nickname",
+                .andDo(document("member/signup/profile/fail/birthday",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestHeaders(
+                                headerWithName("Authorization").description("엑세스 토큰")
+                        ),
+                        requestFields(
+                                fieldWithPath("nickname").description("닉네임"),
+                                fieldWithPath("birthday").description("생년월일. 형식:yyyy-MM-dd"),
+                                fieldWithPath("genderType").description("성별. 형식: MEN 또는 WOMEN"),
+                                fieldWithPath("profileImageUrl").description("프로필 이미지 경로.")
+                        )
+                ))
+                .andExpect(status().isBadRequest());
+    }
+
+    @DisplayName("프로필 정보로 회원가입시 입력한 성별이 유효하지 않다면 상태코드 400을 리턴한다.")
+    @Test
+    void 프로필_정보로_회원가입시_입력한_성별이_유효하지_않다면_상태코드_400을_리턴한다() throws Exception {
+        // given
+        given(jwtTokenProvider.getMemberId(anyString())).willReturn(1L);
+        given(memberRepository.findById(anyLong())).willReturn(Optional.of(하온_신규()));
+        doThrow(new InvalidGenderFormatException("유효하지 않은 성별 입니다."))
+                .when(memberService).signUpByProfile(anyLong(), any());
+
+        // when, then
+        mockMvc.perform(post("/api/member/signup/profile")
+                        .header("Authorization", "Bearer aaaaaa.bbbbbb.cccccc")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(유효하지_않은_닉네임_프로필_정보로_회원가입_요청()))
+                )
+                .andDo(print())
+                .andDo(document("member/signup/profile/fail/genderType",
                         preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()),
                         requestHeaders(
