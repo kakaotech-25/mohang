5e4b463608f56d6456c3454af726b83392f4ac89
msung99 <msung6924@naver.com>
2024-08-27 12:00:55 +0900

    #147 test: 일정에 담겨있는 여행지 리스트를 찾는지 테스트

--- backend/src/test/java/moheng/planner/domain/TripScheduleRepositoryTest.java
+++ backend/src/test/java/moheng/planner/domain/TripScheduleRepositoryTest.java
@@ -9,6 +9,9 @@ import moheng.config.slice.RepositoryTestConfig;
 import moheng.member.domain.Member;
 import moheng.member.domain.repository.MemberRepository;
 import moheng.planner.dto.FindPlannerOrderByRecentResponse;
+import moheng.planner.dto.FindTripsOnSchedule;
+import moheng.trip.domain.Trip;
+import moheng.trip.domain.TripRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +25,12 @@ public class TripScheduleRepositoryTest extends RepositoryTestConfig {
 
     @Autowired
     private MemberRepository memberRepository;
+
+    @Autowired
+    private TripRepository tripRepository;
+
+    @Autowired
+    private TripScheduleRegistryRepository tripScheduleRegistryRepository;
 
     @DisplayName("여행 일정을 생성날짜 기준으로 내림차순 정렬한다.")
     @Test
@@ -92,5 +101,26 @@ public class TripScheduleRepositoryTest extends RepositoryTestConfig {
 
         // when, then
         assertTrue(tripScheduleRepository.existsByMemberAndName(member, "멤버 일정"));
+    }
+
+    @DisplayName("일정에 담겨있는 여행지 리스트를 찾는다.")
+    @Test
+    void 일정에_담겨있는_여행지_리스트를_찾는다() {
+        // given
+        Member member = memberRepository.save(하온_기존());
+        TripSchedule tripSchedule = tripScheduleRepository.save(new TripSchedule("멤버 일정", LocalDate.of(2020, 8, 1), LocalDate.of(2024, 8, 2), member));
+        Trip trip1 = tripRepository.save(new Trip("여행지1", "장소명", 1L, "설명", "https://image.com", 126.3307690830, 36.5309210243));
+        Trip trip2 = tripRepository.save(new Trip("여행지2", "장소명", 1L, "설명", "https://image.com", 226.3307690830, 46.5309210243));
+        tripScheduleRegistryRepository.save(new TripScheduleRegistry(trip1, tripSchedule));
+        tripScheduleRegistryRepository.save(new TripScheduleRegistry(trip2, tripSchedule));
+
+        tripScheduleRegistryRepository.save(new TripScheduleRegistry(trip1, tripSchedule));
+        tripScheduleRegistryRepository.save(new TripScheduleRegistry(trip2, tripSchedule));
+
+        // when
+        List<Trip> trips = tripRepository.findTripsByScheduleId(tripSchedule.getId());
+
+        // then
+        assertThat(trips.size()).isEqualTo(2);
     }
 }
