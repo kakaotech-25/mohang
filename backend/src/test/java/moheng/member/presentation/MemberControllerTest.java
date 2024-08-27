7ae1cc5b3d782ad37538da44f8bfd22541aff228
msung99 <msung6924@naver.com>
2024-08-27 22:50:27 +0900

    #178 test: 프로필 정보로 회원가입시 입력한 닉네임의 길이가 유효범위를 벗어나면 상태코드 400을 리턴하는지 테스트

--- backend/src/test/java/moheng/member/presentation/MemberControllerTest.java
+++ backend/src/test/java/moheng/member/presentation/MemberControllerTest.java
@@ -8,6 +8,7 @@ import moheng.config.slice.ControllerTestConfig;
 import moheng.liveinformation.exception.EmptyLiveInformationException;
 import moheng.liveinformation.exception.NoExistLiveInformationException;
 import moheng.member.exception.DuplicateNicknameException;
+import moheng.member.exception.InvalidNicknameFormatException;
 import moheng.member.exception.NoExistMemberException;
 import moheng.member.exception.ShortContentidsSizeException;
 import org.junit.jupiter.api.DisplayName;
@@ -86,6 +87,39 @@ public class MemberControllerTest extends ControllerTestConfig {
                         )
                 ))
                 .andExpect(status().isNotFound());
+    }
+
+    @DisplayName("프로필 정보로 회원가입시 입력한 닉네임의 길이가 유효범위를 벗어나면 상태코드 400을 리턴한다.")
+    @Test
+    void 프로필_정보로_회원가입시_입력한_닉네임의_길이가_유효범위를_벗어나면_상태코드_400을_리턴한다() throws Exception {
+        // given
+        given(jwtTokenProvider.getMemberId(anyString())).willReturn(1L);
+        given(memberRepository.findById(anyLong())).willReturn(Optional.of(하온_신규()));
+        doThrow(new InvalidNicknameFormatException("이름은 1자 이상 50이하만 허용합니다."))
+                .when(memberService).signUpByProfile(anyLong(), any());
+
+        // when, then
+        mockMvc.perform(post("/api/member/signup/profile")
+                        .header("Authorization", "Bearer aaaaaa.bbbbbb.cccccc")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(유효하지_않은_닉네임_프로필_정보로_회원가입_요청()))
+                )
+                .andDo(print())
+                .andDo(document("member/signup/profile/success",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestHeaders(
+                                headerWithName("Authorization").description("엑세스 토큰")
+                        ),
+                        requestFields(
+                                fieldWithPath("nickname").description("닉네임"),
+                                fieldWithPath("birthday").description("생년월일. 형식:yyyy-MM-dd"),
+                                fieldWithPath("genderType").description("성별. 형식: MEN 또는 WOMEN"),
+                                fieldWithPath("profileImageUrl").description("프로필 이미지 경로.")
+                        )
+                ))
+                .andExpect(status().isBadRequest());
     }
 
     @DisplayName("프로필 정보로 회원가입에 성공하면 상태코드 204을 리턴한다.")
