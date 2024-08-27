83a91905e56f1511fbbf1bbae141c71c5f471944
msung99 <msung6924@naver.com>
2024-08-27 15:20:39 +0900

    #147 test: 세부 일정내 여행지 리스트 정렬 순서를 변경하고 상태코드 204을 리턴하는지 테스트

--- backend/src/test/java/moheng/acceptance/TripScheduleAcceptenceTest.java
+++ backend/src/test/java/moheng/acceptance/TripScheduleAcceptenceTest.java
@@ -2,6 +2,7 @@ package moheng.acceptance;
 
 import static moheng.acceptance.fixture.TripScheduleAcceptenceTestFixture.*;
 import static moheng.acceptance.fixture.AuthAcceptanceFixture.자체_토큰을_생성한다;
+import static moheng.acceptance.fixture.TripScheduleAcceptenceTestFixture.세부_일정을_찾는다;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -15,12 +16,14 @@ import moheng.acceptance.config.AcceptanceTestConfig;
 import moheng.auth.dto.AccessTokenResponse;
 import moheng.planner.dto.CreateTripScheduleRequest;
 import moheng.planner.dto.FindTripsOnSchedule;
+import moheng.planner.dto.UpdateTripOrdersRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class TripScheduleAcceptenceTest extends AcceptanceTestConfig {
 
@@ -91,13 +94,58 @@ public class TripScheduleAcceptenceTest extends AcceptanceTestConfig {
 
         // when
         ExtractableResponse<Response> resultResponse = 세부_일정을_찾는다(1L, accessTokenResponse);
-
         FindTripsOnSchedule findTripsOnSchedule = resultResponse.as(FindTripsOnSchedule.class);
 
         // then
         assertAll(() -> {
             assertThat(resultResponse.statusCode()).isEqualTo(200);
             assertThat(findTripsOnSchedule.getFindTripsOnSchedules()).hasSize(3);
+        });
+    }
+
+    @DisplayName("세부 일정내 여행지 리스트 정렬 순서를 변경하고 상태코드 204을 리턴한다.")
+    @Test
+    void 세부_일전애_여행지_리스트_정렬_순서를_변경하고_상태코드_204를_리턴한다() {
+        // given
+        ExtractableResponse<Response> loginResponse = 자체_토큰을_생성한다("KAKAO", "authorization-code");
+        AccessTokenResponse accessTokenResponse = loginResponse.as(AccessTokenResponse.class);
+
+        플래너에_여행_일정을_생성한다(
+                accessTokenResponse,
+                new CreateTripScheduleRequest("여행 일정1",
+                        LocalDate.of(2020, 1, 1),
+                        LocalDate.of(2022, 9, 10))
+        );
+        여행지를_생성한다("여행지1", 1L);
+        여행지를_생성한다("여행지2", 2L);
+        여행지를_생성한다("여행지3", 3L);
+
+        플래너에_여행지를_담는다(accessTokenResponse, 1L, 1L);
+        플래너에_여행지를_담는다(accessTokenResponse, 2L, 1L);
+        플래너에_여행지를_담는다(accessTokenResponse, 3L, 1L);
+
+        // when
+        long scheduleId = 1L;
+
+        ExtractableResponse<Response> resultResponse = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .auth().oauth2(accessTokenResponse.getAccessToken())
+                .body(new UpdateTripOrdersRequest(List.of(3L, 1L, 2L)))
+                .when().post("/api/schedule/trips/orders/{scheduleId}", scheduleId)
+                .then().log().all()
+                .statusCode(HttpStatus.NO_CONTENT.value())
+                .extract();
+
+
+        FindTripsOnSchedule findTripsOnSchedule = 세부_일정을_찾는다(scheduleId, accessTokenResponse).as(FindTripsOnSchedule.class);
+
+        // then
+        assertAll(() -> {
+            assertThat(resultResponse.statusCode()).isEqualTo(204);
+            assertThat(findTripsOnSchedule.getFindTripsOnSchedules()).hasSize(3);
+            assertThat(findTripsOnSchedule.getFindTripsOnSchedules().get(0).getPlaceName()).isEqualTo("여행지3");
+            assertThat(findTripsOnSchedule.getFindTripsOnSchedules().get(1).getPlaceName()).isEqualTo("여행지1");
+            assertThat(findTripsOnSchedule.getFindTripsOnSchedules().get(2).getPlaceName()).isEqualTo("여행지2");
         });
     }
 }
