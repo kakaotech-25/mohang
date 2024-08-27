97e55239174f9b90a70fa834f5974768597a185b
msung99 <msung6924@naver.com>
2024-08-27 13:39:26 +0900

    #147 feat: 세부 일정 API 컨트롤러 작성

--- backend/src/main/java/moheng/planner/presentation/TripScheduleController.java
+++ backend/src/main/java/moheng/planner/presentation/TripScheduleController.java
@@ -4,6 +4,8 @@ import moheng.auth.dto.Accessor;
 import moheng.auth.presentation.authentication.Authentication;
 import moheng.planner.application.TripScheduleService;
 import moheng.planner.dto.CreateTripScheduleRequest;
+import moheng.planner.dto.FindTripsOnSchedule;
+import moheng.planner.dto.UpdateTripOrdersRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,6 +30,20 @@ public class TripScheduleController {
                                                   @PathVariable("tripId") final Long tripId,
                                                   @PathVariable("scheduleId") final Long scheduleId) {
         tripScheduleService.addCurrentTripOnPlannerSchedule(tripId, scheduleId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/trips/{scheduleId}")
+    public ResponseEntity<FindTripsOnSchedule> findTripsOnSchedule(@Authentication final Accessor accessor,
+                                                                   @PathVariable("scheduleId") final Long scheduleId) {
+        return ResponseEntity.ok(tripScheduleService.findTripsOnSchedule(scheduleId));
+    }
+
+    @PostMapping("/trips/orders/{scheduleId}")
+    public ResponseEntity<Void> updateTripOrdersOnSchedule(@Authentication final Accessor accessor,
+                                                           @PathVariable("scheduleId") final Long scheduleId,
+                                                           @RequestBody UpdateTripOrdersRequest updateTripOrdersRequest) {
+        tripScheduleService.updateTripOrdersOnSchedule(scheduleId, updateTripOrdersRequest);
         return ResponseEntity.noContent().build();
     }
 }
