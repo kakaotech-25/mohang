edb26f736e4264ab674616e618f92b0d4d6f538a
msung99 <msung6924@naver.com>
2024-08-27 15:26:41 +0900

    #147 test: 세부 일정내 여행지를 제거하고 상태코드 204를 리턴하는지 인수 테스트

--- backend/src/test/java/moheng/acceptance/TripScheduleAcceptenceTest.java
+++ backend/src/test/java/moheng/acceptance/TripScheduleAcceptenceTest.java
@@ -105,7 +105,7 @@ public class TripScheduleAcceptenceTest extends AcceptanceTestConfig {
 
     @DisplayName("세부 일정내 여행지 리스트 정렬 순서를 변경하고 상태코드 204을 리턴한다.")
     @Test
-    void 세부_일전애_여행지_리스트_정렬_순서를_변경하고_상태코드_204를_리턴한다() {
+    void 세부_일정내_여행지_리스트_정렬_순서를_변경하고_상태코드_204를_리턴한다() {
         // given
         ExtractableResponse<Response> loginResponse = 자체_토큰을_생성한다("KAKAO", "authorization-code");
         AccessTokenResponse accessTokenResponse = loginResponse.as(AccessTokenResponse.class);
@@ -136,6 +136,48 @@ public class TripScheduleAcceptenceTest extends AcceptanceTestConfig {
             assertThat(findTripsOnSchedule.getFindTripsOnSchedules().get(0).getPlaceName()).isEqualTo("여행지3");
             assertThat(findTripsOnSchedule.getFindTripsOnSchedules().get(1).getPlaceName()).isEqualTo("여행지1");
             assertThat(findTripsOnSchedule.getFindTripsOnSchedules().get(2).getPlaceName()).isEqualTo("여행지2");
+        });
+    }
+
+    @DisplayName("세부 일정내 여행지를 제거하고 상태코드 204를 리턴한다.")
+    @Test
+    void 세부_일정내_여행지를_제거하고_상태코드_204를_리턴한다() {
+        // given
+        ExtractableResponse<Response> loginResponse = 자체_토큰을_생성한다("KAKAO", "authorization-code");
+        AccessTokenResponse accessTokenResponse = loginResponse.as(AccessTokenResponse.class);
+
+        플래너에_여행_일정을_생성한다(
+                accessTokenResponse,
+                new CreateTripScheduleRequest("여행 일정1",
+                        LocalDate.of(2020, 1, 1),
+                        LocalDate.of(2022, 9, 10))
+        );
+        여행지를_생성한다("여행지1", 1L);
+        여행지를_생성한다("여행지2", 2L);
+        여행지를_생성한다("여행지3", 3L);
+
+        플래너에_여행지를_담는다(accessTokenResponse, 1L, 1L);
+        플래너에_여행지를_담는다(accessTokenResponse, 2L, 1L);
+        플래너에_여행지를_담는다(accessTokenResponse, 3L, 1L);
+
+        // when
+        long scheduleId = 1L;
+        long tripId = 1L;
+
+        ExtractableResponse<Response> resultResponse = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .auth().oauth2(accessTokenResponse.getAccessToken())
+                .when().delete("/api/schedule/{scheduleId}/{tripId}", scheduleId, tripId)
+                .then().log().all()
+                .statusCode(HttpStatus.NO_CONTENT.value())
+                .extract();
+
+        FindTripsOnSchedule findTripsOnSchedule = 세부_일정을_찾는다(1L, accessTokenResponse).as(FindTripsOnSchedule.class);
+
+        // then
+        assertAll(() -> {
+            assertThat(resultResponse.statusCode()).isEqualTo(204);
+            assertThat(findTripsOnSchedule.getFindTripsOnSchedules()).hasSize(2);
         });
     }
 }
