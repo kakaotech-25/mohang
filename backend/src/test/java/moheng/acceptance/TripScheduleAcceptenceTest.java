1c78687c70ac12f83e4e7471ea06c76db5f77b09
msung99 <msung6924@naver.com>
2024-08-27 11:01:10 +0900

    #147 test: 현재 여행지를 플래너 일정에 담고 상태코드 204를 리턴하는지 인수 테스트

--- backend/src/test/java/moheng/acceptance/TripScheduleAcceptenceTest.java
+++ backend/src/test/java/moheng/acceptance/TripScheduleAcceptenceTest.java
@@ -16,6 +16,7 @@ import io.restassured.response.Response;
 import moheng.acceptance.config.AcceptanceTestConfig;
 import moheng.auth.dto.AccessTokenResponse;
 import moheng.planner.dto.CreateTripScheduleRequest;
+import moheng.trip.dto.TripCreateRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
@@ -32,6 +33,7 @@ public class TripScheduleAcceptenceTest extends AcceptanceTestConfig {
         ExtractableResponse<Response> loginResponse = 자체_토큰을_생성한다("KAKAO", "authorization-code");
         AccessTokenResponse accessTokenResponse = loginResponse.as(AccessTokenResponse.class);
 
+        // when
         ExtractableResponse<Response> createScheduleResponse = 플래너에_여행_일정을_생성한다(
                 accessTokenResponse,
                 new CreateTripScheduleRequest("여행 일정1",
@@ -39,9 +41,42 @@ public class TripScheduleAcceptenceTest extends AcceptanceTestConfig {
                         LocalDate.of(2022, 9, 10)
                 ));
 
-        // when, then
+        //  then
         assertAll(() -> {
             assertThat(createScheduleResponse.statusCode()).isEqualTo(204);
+        });
+    }
+
+    @DisplayName("현재 여행지를 플래너 일정에 담고 상태코드 204를 리턴한다.")
+    @Test
+    void 현재_여행지를_플래너_일정에_담고_상태코드_204를_리턴한다() {
+        // given
+        ExtractableResponse<Response> loginResponse = 자체_토큰을_생성한다("KAKAO", "authorization-code");
+        AccessTokenResponse accessTokenResponse = loginResponse.as(AccessTokenResponse.class);
+
+        플래너에_여행_일정을_생성한다(
+                accessTokenResponse,
+                new CreateTripScheduleRequest("여행 일정1",
+                        LocalDate.of(2020, 1, 1),
+                        LocalDate.of(2022, 9, 10))
+        );
+
+        여행지를_생성한다("여행지", 1L);
+
+        long tripId = 1L;
+        long scheduleId = 1L;
+
+        // when
+        ExtractableResponse<Response> resultResponse =RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .auth().oauth2(accessTokenResponse.getAccessToken())
+                .when().post("/api/schedule/trip/{tripId}/{scheduleId}", tripId, scheduleId)
+                .then().log().all()
+                .statusCode(HttpStatus.NO_CONTENT.value())
+                .extract();
+
+        assertAll(() -> {
+            assertThat(resultResponse.statusCode()).isEqualTo(204);
         });
     }
 }
