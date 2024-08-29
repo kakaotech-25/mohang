a253bc02257d9df153329f3088529643b2d0f571
msung99 <msung6924@naver.com>
2024-08-29 13:53:24 +0900

    #185 test: 멤버의 생활정보가 없으면 추가적인 생활정보 기반 필터링이 수행되지 않는지 테스트

--- backend/src/test/java/moheng/recommendtrip/domain/TripPreferredLocationsFilterStrategyTest.java
+++ backend/src/test/java/moheng/recommendtrip/domain/TripPreferredLocationsFilterStrategyTest.java
@@ -145,4 +145,48 @@ public class TripPreferredLocationsFilterStrategyTest extends ServiceTestConfig 
         assertThat(liveInformations)
                 .allMatch(liveInfo -> "생활정보1".equals(liveInfo.getName()));
     }
+
+    @DisplayName("멤버의 생활정보가 없으면 추가적인 생활정보 기반 필터링이 수행되지 않는다.")
+    @Test
+    void 멤버의_생활정보가_없으면_추가적인_생활정보_기반_필터링이_수행되지_않는다() {
+        // given
+        Member member = memberRepository.save(하온_기존());
+        Trip trip1 = tripRepository.save(new Trip("여행지1", "장소명1", 1L, "설명1", "이미지 경로1"));
+        Trip trip2 = tripRepository.save(new Trip("여행지2", "장소명2", 2L, "설명2", "이미지 경로2"));
+        Trip trip3 = tripRepository.save(new Trip("여행지3", "장소명3", 3L, "설명3", "이미지 경로3"));
+        Trip trip4 = tripRepository.save(new Trip("여행지4", "장소명3", 4L, "설명3", "이미지 경로3"));
+        Trip trip5 = tripRepository.save(new Trip("여행지5", "장소명3", 5L, "설명3", "이미지 경로3"));
+        Trip trip6 = tripRepository.save(new Trip("여행지6", "장소명3", 6L, "설명3", "이미지 경로3"));
+        Trip trip7 = tripRepository.save(new Trip("여행지7", "장소명3", 7L, "설명3", "이미지 경로3"));
+        Trip trip8 = tripRepository.save(new Trip("여행지8", "장소명3", 8L, "설명3", "이미지 경로3"));
+        Trip trip9 = tripRepository.save(new Trip("여행지9", "장소명3", 9L, "설명3", "이미지 경로3"));
+        Trip trip10 = tripRepository.save(new Trip("여행지10", "장소명3", 10L, "설명3", "이미지 경로3"));
+        memberTripRepository.save(new MemberTrip(member, trip1, 10L)); memberTripRepository.save(new MemberTrip(member, trip2, 20L));
+        memberTripRepository.save(new MemberTrip(member, trip3, 30L)); memberTripRepository.save(new MemberTrip(member, trip4, 30L));
+        memberTripRepository.save(new MemberTrip(member, trip5, 30L)); memberTripRepository.save(new MemberTrip(member, trip6, 30L));
+        memberTripRepository.save(new MemberTrip(member, trip7, 30L)); memberTripRepository.save(new MemberTrip(member, trip8, 30L));
+        memberTripRepository.save(new MemberTrip(member, trip9, 30L)); memberTripRepository.save(new MemberTrip(member, trip10, 30L));
+        recommendTripRepository.save(new RecommendTrip(trip1, member, 1L)); recommendTripRepository.save(new RecommendTrip(trip2, member, 2L));
+        recommendTripRepository.save(new RecommendTrip(trip3, member, 3L)); recommendTripRepository.save(new RecommendTrip(trip4, member, 4L));
+        recommendTripRepository.save(new RecommendTrip(trip5, member, 5L)); recommendTripRepository.save(new RecommendTrip(trip6, member, 6L));
+        recommendTripRepository.save(new RecommendTrip(trip7, member, 7L)); recommendTripRepository.save(new RecommendTrip(trip8, member, 8L));
+        recommendTripRepository.save(new RecommendTrip(trip9, member, 9L)); recommendTripRepository.save(new RecommendTrip(trip10, member, 10L));
+        LiveInformation liveInformation1 = liveInformationRepository.save(new LiveInformation("생활정보1"));
+        LiveInformation liveInformation2 = liveInformationRepository.save(new LiveInformation("생활정보2"));
+        tripLiveInformationRepository.save(new TripLiveInformation(liveInformation1, trip1)); tripLiveInformationRepository.save(new TripLiveInformation(liveInformation1, trip2));
+        tripLiveInformationRepository.save(new TripLiveInformation(liveInformation1, trip3)); tripLiveInformationRepository.save(new TripLiveInformation(liveInformation1, trip4));
+        tripLiveInformationRepository.save(new TripLiveInformation(liveInformation1, trip5)); tripLiveInformationRepository.save(new TripLiveInformation(liveInformation1, trip6));
+        tripLiveInformationRepository.save(new TripLiveInformation(liveInformation1, trip7)); tripLiveInformationRepository.save(new TripLiveInformation(liveInformation1, trip8));
+        tripLiveInformationRepository.save(new TripLiveInformation(liveInformation2, trip9)); tripLiveInformationRepository.save(new TripLiveInformation(liveInformation2, trip10));
+
+        // when
+        List<Trip> trips = tripPreferredLocationsFilterStrategy.execute(new PreferredLocationsFilterInfo(member.getId()));
+
+        // then
+        List<LiveInformation> liveInformations = tripLiveInformationRepository.findLiveInformationByTrips(trips);
+        
+        assertThat(trips).hasSize(10); // 추천받은 모든 여행지가 포함됨
+        assertThat(liveInformations).hasSize(10); // 각 여행지의 생활정보가 포함됨
+
+    }
 }
