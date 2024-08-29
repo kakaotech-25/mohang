675446f8f9b93829031da5a94677c66683b50a04
msung99 <msung6924@naver.com>
2024-08-29 12:00:31 +0900

    #185 feat: 불필요한 임포트문 제거

--- backend/src/main/java/moheng/recommendtrip/domain/tripfilterstrategy/TripFilterStrategyProvider.java
+++ backend/src/main/java/moheng/recommendtrip/domain/tripfilterstrategy/TripFilterStrategyProvider.java
@@ -1,23 +1,7 @@
 package moheng.recommendtrip.domain.tripfilterstrategy;
 
-import moheng.liveinformation.domain.LiveInformation;
-import moheng.liveinformation.domain.MemberLiveInformationRepository;
-import moheng.liveinformation.domain.TripLiveInformationRepository;
-import moheng.trip.domain.ExternalRecommendModelClient;
-import moheng.trip.domain.ExternalSimilarTripModelClient;
-import moheng.trip.domain.Trip;
-import moheng.trip.domain.TripRepository;
-import moheng.trip.dto.RecommendTripsByVisitedLogsRequest;
-import moheng.trip.dto.RecommendTripsByVisitedLogsResponse;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-// 필터 전략 2가지 종류
-// 1. 선호 여행지 (+ 멤버 생활정보)  (-> AI 맞춤 추천 여행지)  / 2. 생활정보 (-> 비슷한 여행지)
 
 @Component
 public class TripFilterStrategyProvider {
