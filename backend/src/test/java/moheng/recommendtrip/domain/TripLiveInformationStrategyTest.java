7ef125759e465273ca0b6c8f64f9d7a4b5b16236
msung99 <msung6924@naver.com>
2024-08-29 12:13:45 +0900

    #185 test: 존재하지 않는 여행지에 기반하여 생활정보를 필터링하려고 하면 예외가 발생하는지 테스트

--- backend/src/test/java/moheng/recommendtrip/domain/TripLiveInformationStrategyTest.java
+++ backend/src/test/java/moheng/recommendtrip/domain/TripLiveInformationStrategyTest.java
@@ -4,7 +4,10 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import moheng.config.slice.ServiceTestConfig;
+import moheng.recommendtrip.domain.filterinfo.FilterStandardInfo;
+import moheng.recommendtrip.domain.filterinfo.LiveInformationFilterInfo;
 import moheng.recommendtrip.domain.tripfilterstrategy.TripLiveInformationFilterStrategy;
+import moheng.trip.exception.NoExistTripException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +24,17 @@ public class TripLiveInformationStrategyTest extends ServiceTestConfig {
 
         // when, then
         assertTrue(() -> tripLiveInformationFilterStrategy.isMatch(LIVE_INFO));
+    }
+
+    @DisplayName("존재하지 않는 여행지에 기반하여 생활정보를 필터링하려고 하면 예외가 발생한다.")
+    @Test
+    void 존재하지_않는_여행지에_기반하여_생활정보를_필터링하려고_하면_예외가_발생한다() {
+        // given
+        long invalidTripId = -1L;
+        FilterStandardInfo filterStandardInfo = new LiveInformationFilterInfo(invalidTripId);
+
+        // when, then
+        assertThatThrownBy(() -> tripLiveInformationFilterStrategy.execute(filterStandardInfo))
+                .isInstanceOf(NoExistTripException.class);
     }
 }
