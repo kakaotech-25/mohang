2bb993df2f25538a7ae407993d40c601caf03f7d
msung99 <msung6924@naver.com>
2024-08-31 20:01:09 +0900

    #223 feat: 여행지 더미데이터 생성

--- backend/src/main/java/moheng/eventlistener/TripApplicationRunner.java
+++ backend/src/main/java/moheng/eventlistener/TripApplicationRunner.java
@@ -1,5 +1,8 @@
 package moheng.eventlistener;
 
+import moheng.keyword.domain.Keyword;
+import moheng.keyword.domain.TripKeyword;
+import moheng.trip.domain.Trip;
 import moheng.trip.domain.TripRepository;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -10,10 +13,49 @@ import org.springframework.stereotype.Component;
 @Order(2)
 @Component
 public class TripApplicationRunner implements ApplicationRunner {
-    private TripRepository tripRepository;
+    private final TripRepository tripRepository;
+
+    public TripApplicationRunner(final TripRepository tripRepository) {
+        this.tripRepository = tripRepository;
+    }
 
     @Override
     public void run(ApplicationArguments args) {
+        tripRepository.save(new Trip("여행지1", "서울특별시 송파구1", 1L, "여행지 설명1",
+                "https://lotte-world.ong"));
+
+        tripRepository.save(new Trip("여행지2", "서울특별시 송파구2", 2L, "여행지 설명2",
+                "https://lotte-world.ong"));
+
+        tripRepository.save(new Trip("여행지3", "서울특별시 송파구3", 3L, "여행지 설명3",
+                "https://lotte-world.ong"));
+
+        tripRepository.save(new Trip("여행지4", "서울특별시 송파구4", 4L, "여행지 설명4",
+                "https://lotte-world.ong"));
+
+        tripRepository.save(new Trip("여행지5", "서울특별시 송파구5", 5L, "여행지 설명5",
+                "https://lotte-world.ong"));
+
+        tripRepository.save(new Trip("여행지6", "서울특별시 송파구6", 6L, "여행지 설명6",
+                "https://lotte-world.ong"));
+
+        tripRepository.save(new Trip("여행지7", "서울특별시 송파구7", 7L, "여행지 설명7",
+                "https://lotte-world.ong"));
+
+        tripRepository.save(new Trip("여행지8", "서울특별시 송파구8", 8L, "여행지 설명8",
+                "https://lotte-world.ong"));
+
+        tripRepository.save(new Trip("여행지9", "서울특별시 송파구9", 9L, "여행지 설명9",
+                "https://lotte-world.ong"));
+
+        tripRepository.save(new Trip("여행지10", "서울특별시 송파구10", 10L, "여행지 설명10",
+                "https://lotte-world.ong"));
+
+        tripRepository.save(new Trip("여행지11", "서울특별시 송파구11", 11L, "여행지 설명11",
+                "https://lotte-world.ong"));
+
+        tripRepository.save(new Trip("여행지12", "서울특별시 송파구12", 12L, "여행지 설명12",
+                "https://lotte-world.ong"));
 
     }
 }
