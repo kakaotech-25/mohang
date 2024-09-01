c6733ce6c9e5568a2b8152343707bd374a009d7c
Daeun Lee <56716976+LEE-DA-EUN@users.noreply.github.com>
2024-09-01 23:32:14 +0900

    #219 feat: 데모영상보기 버튼을 누르면 아래로 스크롤하여 영상화면으로 넘어가는 기능 구현

--- frontend/src/pages/Landing/Landing.jsx
+++ frontend/src/pages/Landing/Landing.jsx
@@ -1,12 +1,29 @@
-import './Landing.css'
+import React, { useRef } from 'react';
+import './Landing.css';
 import MainImg from "../../assets/landing-img.png";
 
 const Landing = () => {
-    return (
-      <div className="landing-container">
-        <img src={MainImg} alt="Landing" className="landing-image" />
-      </div>
-    );
+  const secondSectionRef = useRef(null);
+
+  const handleScroll = () => {
+    secondSectionRef.current.scrollIntoView({ behavior: 'smooth' });
   };
-  
-  export default Landing;
+
+  return (
+    <div>
+      <div className="landing-container">
+        <div className="landing-content">
+          <h1 className="landing-title">모두가 행복한 여행지로 안내합니다.</h1>
+          <button className="landing-button" onClick={handleScroll}>
+            데모 영상 보기
+          </button>
+        </div>
+      </div>
+      <div ref={secondSectionRef} className="second-section">
+        <h2>영상 공간</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
