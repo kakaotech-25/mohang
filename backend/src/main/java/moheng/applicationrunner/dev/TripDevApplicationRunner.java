0246f338f4a6b0b167dcf3549ef9973e7c16e34b
msung99 <msung6924@naver.com>
2024-09-02 22:43:15 +0900

    #123 feat; JSON 파일로부터 여행지 정보를 읽어오는 기능 구현

--- backend/src/main/java/moheng/applicationrunner/dev/TripDevApplicationRunner.java
+++ backend/src/main/java/moheng/applicationrunner/dev/TripDevApplicationRunner.java
@@ -3,6 +3,7 @@ package moheng.applicationrunner.dev;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import moheng.applicationrunner.dto.LiveInformationRunner;
+import moheng.applicationrunner.dto.TripRunner;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.annotation.Order;
@@ -12,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Order(9)
+@Order(10)
 @Component
 public class TripDevApplicationRunner implements ApplicationRunner {
 
@@ -20,6 +21,10 @@ public class TripDevApplicationRunner implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         final Resource resource = new ClassPathResource("triptmp.json");
         final ObjectMapper objectMapper = new ObjectMapper();
-        final List<LiveInformationRunner> liveInformationRunners = objectMapper.readValue(resource.getInputStream(), new TypeReference<List<LiveInformationRunner>>() {});
+        final List<TripRunner> tripRunners = objectMapper.readValue(resource.getInputStream(), new TypeReference<List<TripRunner>>() {});
+
+        for(TripRunner tripRunner : tripRunners) {
+            System.out.println(tripRunner);
+        }
     }
 }
--- backend/src/main/java/moheng/applicationrunner/dto/TripRunner.java
+++ backend/src/main/java/moheng/applicationrunner/dto/TripRunner.java
@@ -1,7 +1,7 @@
 package moheng.applicationrunner.dto;
 
 public class TripRunner {
-    private String contentid;
+    private Long contentid;
     private String contenttypeid;
     private String title;
     private String createdtime;
@@ -15,29 +15,77 @@ public class TripRunner {
     private String cpyrhtDivCd;
     private String areacode;
     private String sigungucode;
-    private String mapx;
-    private String mapy;
+    private Double mapx;
+    private Double mapy;
     private String mlevel;
     private String overview;
 
-    public String getContentid() {
+    public Long getContentid() {
         return contentid;
+    }
+
+    public String getContenttypeid() {
+        return contenttypeid;
     }
 
     public String getTitle() {
         return title;
     }
 
+    public String getCreatedtime() {
+        return createdtime;
+    }
+
+    public String getModifiedtime() {
+        return modifiedtime;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public String getTelname() {
+        return telname;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public String getBooktour() {
+        return booktour;
+    }
+
     public String getFirstimage() {
         return firstimage;
     }
 
-    public String getMapx() {
+    public String getFirstimage2() {
+        return firstimage2;
+    }
+
+    public String getCpyrhtDivCd() {
+        return cpyrhtDivCd;
+    }
+
+    public String getAreacode() {
+        return areacode;
+    }
+
+    public String getSigungucode() {
+        return sigungucode;
+    }
+
+    public Double getMapx() {
         return mapx;
     }
 
-    public String getMapy() {
+    public Double getMapy() {
         return mapy;
+    }
+
+    public String getMlevel() {
+        return mlevel;
     }
 
     public String getOverview() {
