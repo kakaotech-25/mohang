61a7b6b4e796908fa196c7bae11f0c9057d91456
Daeun Lee <56716976+LEE-DA-EUN@users.noreply.github.com>
2024-09-03 10:48:05 +0900

    #256 feat: 유저데이터 생성

---
+++ frontend/src/data/UserData.jsx
@@ -0,0 +1,11 @@
+import profileimg from "../../assets/profileimg.png";
+
+const UserData = {
+    nickname: "devhaon",
+    birthday: "1999-09-20",
+    genderType: "MEN",
+    profileImageUrl: profileimg
+};
+
+export default UserData;
+  
