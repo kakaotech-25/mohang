e98a75d490871aacb15bef7c0779f87f684f909d
msung99 <msung6924@naver.com>
2024-09-05 14:55:36 +0900

    #251 feat: 프로덕션 더미데이터를 위한 컴포넌트 미리 생성

---
+++ backend/src/main/java/moheng/applicationrunner/prod/MemberLiveInformationTestApplicationRunner.java
@@ -0,0 +1,29 @@
+package moheng.applicationrunner.prod;
+
+import moheng.liveinformation.domain.LiveInformationRepository;
+import moheng.liveinformation.domain.MemberLiveInformationRepository;
+import moheng.member.domain.repository.MemberRepository;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+
+public class MemberLiveInformationTestApplicationRunner implements ApplicationRunner {
+    private final MemberLiveInformationRepository memberLiveInformationRepository;
+    private final MemberRepository memberRepository;
+    private final LiveInformationRepository liveInformationRepository;
+
+    public MemberLiveInformationTestApplicationRunner(final MemberLiveInformationRepository memberLiveInformationRepository,
+                                                      final MemberRepository memberRepository,
+                                                      final LiveInformationRepository liveInformationRepository) {
+        this.memberLiveInformationRepository = memberLiveInformationRepository;
+        this.memberRepository = memberRepository;
+        this.liveInformationRepository = liveInformationRepository;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+
+    }
+}
---
+++ backend/src/main/java/moheng/applicationrunner/prod/TripDevApplicationRunner.java
@@ -0,0 +1,56 @@
+package moheng.applicationrunner.prod;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import moheng.applicationrunner.dto.TripRunner;
+import moheng.trip.domain.Trip;
+import moheng.trip.domain.TripRepository;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class TripDevApplicationRunner implements ApplicationRunner {
+    private final TripRepository tripRepository;
+
+    public TripDevApplicationRunner(final TripRepository tripRepository) {
+        this.tripRepository = tripRepository;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        if(tripRepository.count() == 0) {
+            final Resource resource1 = new ClassPathResource("trip1.json");
+            final Resource resource2 = new ClassPathResource("trip2.json");
+            final ObjectMapper objectMapper = new ObjectMapper();
+
+            final List<TripRunner> tripRunners = new ArrayList<>();
+            tripRunners.addAll(findTripRunnersByResource(resource1, objectMapper));
+            tripRunners.addAll(findTripRunnersByResource(resource2, objectMapper));
+
+            for(final TripRunner tripRunner : tripRunners) {
+                final Long contentId = tripRunner.getContentid();
+                final String title = tripRunner.getTitle();
+                final String placeName = tripRunner.getArea_sigungu_combined();
+                final String tripImageUrl = tripRunner.getFirstimage();
+                final Double mapX = tripRunner.getMapx();
+                final Double mapY = tripRunner.getMapy();
+                final String description = tripRunner.getOverview();
+
+                tripRepository.save(new Trip(title, placeName, contentId, description, tripImageUrl, mapX, mapY));
+            }
+        }
+        System.out.println(tripRepository.count());
+    }
+
+    private List<TripRunner> findTripRunnersByResource(final Resource resource, final ObjectMapper objectMapper) throws IOException {
+        return objectMapper.readValue(resource.getInputStream(), new TypeReference<List<TripRunner>>() {});
+    }
+}
