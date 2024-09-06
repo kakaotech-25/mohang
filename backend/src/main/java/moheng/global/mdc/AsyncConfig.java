0254a879f9b22943ca8c07b6f288df626ab3b9cc
msung99 <msung6924@naver.com>
2024-09-06 11:33:49 +0900

    #163 refactor: MDC 로 로그에 고유한 아이디가 출력되게 변경

---
+++ backend/src/main/java/moheng/global/mdc/AsyncConfig.java
@@ -0,0 +1,20 @@
+package moheng.global.mdc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+    @Bean
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setCorePoolSize(10);
+
+        threadPoolTaskExecutor.setTaskDecorator(new MdcTaskDecorator());
+        return threadPoolTaskExecutor;
+    }
+}
