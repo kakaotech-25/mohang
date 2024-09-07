28e84fbd79a53b404dd5f414f399dec06708469b
msung99 <msung6924@naver.com>
2024-09-07 13:06:20 +0900

    #221 refactor: 메인 페이지에서 키워드로 필터링한 여행지 조회시 발생하는 N+1 문제 해결

--- backend/src/main/java/moheng/keyword/domain/repository/TripKeywordRepository.java
+++ backend/src/main/java/moheng/keyword/domain/repository/TripKeywordRepository.java
@@ -2,6 +2,7 @@ package moheng.keyword.domain.repository;
 
 import moheng.keyword.domain.TripKeyword;
 import moheng.trip.domain.Trip;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,8 +10,11 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface TripKeywordRepository extends JpaRepository<TripKeyword, Long> {
+
+    @EntityGraph(attributePaths = {"trip"})
     @Query("SELECT tk FROM TripKeyword tk WHERE tk.keyword.id IN :keywordIds")
     List<TripKeyword> findTripKeywordsByKeywordIds(@Param("keywordIds") final List<Long> keywordIds);
+
 
     @Query("SELECT tk FROM TripKeyword tk WHERE tk.keyword.id = :keywordId")
     List<TripKeyword> findTripKeywordsByKeywordId(@Param("keywordId") final Long keywordId);
