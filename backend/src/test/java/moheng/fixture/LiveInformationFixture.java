113303d074a99bbfcd8a9999642f63c60fa575f2
msung99 <msung6924@naver.com>
2024-09-09 14:20:16 +0900

    #275 fix: h2 임시 전환

--- backend/src/test/java/moheng/fixture/LiveInformationFixture.java
+++ backend/src/test/java/moheng/fixture/LiveInformationFixture.java
@@ -1,4 +1,9 @@
 package moheng.fixture;
 
+import moheng.liveinformation.domain.LiveInformation;
+
 public class LiveInformationFixture {
+    public static LiveInformation 생활정보1() {
+        return new LiveInformation("생활정보1");
+    }
 }
