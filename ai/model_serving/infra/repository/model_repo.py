007482fdbfd3ceb576bec283ae7ad6be99b2a986
Do-yoon <1.kim.doyoon@gmail.com>
2024-09-09 21:51:46 +0900

    #317 feat: 데이터 수정 및 추천 알고리즘 수정

--- ai/model_serving/infra/repository/model_repo.py
+++ ai/model_serving/infra/repository/model_repo.py
@@ -1,5 +1,5 @@
 from model_serving.domain.repository.model_repo import IModelRepository
-from model.Recommendation import personalized_recommendation, UserPreferences
+from model.Recommendation import recommend_by_user_preference, recommend_by_content_similarity, df
 from typing import List
 from model_serving.interface.model.location import PreferredLocation
 
@@ -11,8 +11,11 @@ class ModelRepository(IModelRepository):
             page: int,
     ) -> list[int]:
         # TODO: 방문 컨텐츠 리스트와 방문 수를 반영한 추천 모듈 실행
-        return personalized_recommendation(UserPreferences(preferred_destinations=list([preferred_location[0].contentId])))\
-        # return list([preferred_location[0].contentId])
+        pl = dict()
+        for e in preferred_location:
+            pl[e.contentId] = e.clicked
+        recommended_ids_preference = recommend_by_user_preference(pl, df, top_n=20, random_n=20)
+        return recommended_ids_preference
 
 
     def similar_content(
@@ -20,5 +23,5 @@ class ModelRepository(IModelRepository):
             contentId: int,
     ) -> list[int]:
         # TODO: 단순히 해당 컨텐츠와 관련된 여행지를 추천
-        return personalized_recommendation(UserPreferences(preferred_destinations=[contentId]))
-        # return [contentId]
+        recommended_ids_similarity = recommend_by_content_similarity(contentId, df, top_n=20, random_n=20)
+        return recommended_ids_similarity
