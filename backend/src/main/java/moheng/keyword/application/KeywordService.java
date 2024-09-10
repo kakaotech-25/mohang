7a66039fae8f454ecdeb6a1abdc711786472c4a9
msung99 <msung6924@naver.com>
2024-09-10 15:45:48 +0900

    #339 feat: 빈 배열 키워드일때의 로직 메소드 분리

--- backend/src/main/java/moheng/keyword/application/KeywordService.java
+++ backend/src/main/java/moheng/keyword/application/KeywordService.java
@@ -63,14 +63,18 @@ public class KeywordService {
 
     public FindTripsResponse findRecommendTripsByKeywords(final TripsByKeyWordsRequest request) {
         if (request.getKeywordIds().isEmpty()) {
-            final Keyword randomKeyword = randomKeywordGeneratable.generate();
-            final List<TripKeyword> tripKeywords = tripKeywordRepository.findTop30ByKeywordId(randomKeyword.getId());
-            final List<Trip> topTrips = tripsByStatisticsFinder.findTripsWithVisitedCount(tripKeywords);
-            return new FindTripsResponse(extractAllTripKeywordsByTopTrips(topTrips));
+            return findRecommendTripsByEmptyKeyword();
         }
         validateKeywords(request.getKeywordIds());
         final List<TripKeyword> trips = filterTop30Trips(tripKeywordRepository.findTripKeywordsByKeywordIds(request.getKeywordIds()));
         return new FindTripsResponse(trips);
+    }
+
+    private FindTripsResponse findRecommendTripsByEmptyKeyword() {
+        final Keyword randomKeyword = randomKeywordGeneratable.generate();
+        final List<TripKeyword> tripKeywords = tripKeywordRepository.findTop30ByKeywordId(randomKeyword.getId());
+        final List<Trip> topTrips = tripsByStatisticsFinder.findTripsWithVisitedCount(tripKeywords);
+        return new FindTripsResponse(extractAllTripKeywordsByTopTrips(topTrips));
     }
 
     private List<TripKeyword> filterTop30Trips(final List<TripKeyword> trips) {
