771233948a78e132094acdab5bdc73f46b7443d6
jaejunday <wowns8114@gmail.com>
2024-09-10 16:28:03 +0900

    #238 feat: planner 페이지 지도 마커, 일정 불러오기 api연동

--- frontend/src/pages/PlannerDetails/PlannerDetails.jsx
+++ frontend/src/pages/PlannerDetails/PlannerDetails.jsx
@@ -1,34 +1,80 @@
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import { useParams, useNavigate } from 'react-router-dom';
-import PlannerData from '../../data/PlannerData';
-import './PlannerDetails.css';
-import { useState, useEffect } from 'react';
-import deleteIcon from '../../assets/delete-icon.png';
-import backIcon from '../../assets/back-icon.png';
-import editIcon from '../../assets/editicon.png';
-import PlannerModal from '../../components/PlannerModal/PlannerModal';
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect, useLayoutEffect } from "react";
+import deleteIcon from "../../assets/delete-icon.png";
+import backIcon from "../../assets/back-icon.png";
+import editIcon from "../../assets/editicon.png";
+import PlannerModal from "../../components/PlannerModal/PlannerModal";
+import axiosInstance from "../login/axiosInstance";
+import "./PlannerDetails.css";
 
 const PlannerDetails = () => {
   const { id } = useParams();
-  const plan = PlannerData.find((p) => p.id === parseInt(id));
-  const [destinations, setDestinations] = useState(plan.destinations);
+  const [scheduleName, setScheduleName] = useState("");
+  const [schedulePeriod, setSchedulePeriod] = useState("");
+  const [destinations, setDestinations] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedDestination, setSelectedDestination] = useState(null);
   const navigate = useNavigate();
 
+  // eslint-disable-next-line no-undef
+  useLayoutEffect(() => {
+    const fetchPlannerData = async () => {
+      try {
+        const response = await axiosInstance.get(`/schedule/trips/${id}`);
+        const tripData = response.data;
+        // const tripData = {
+        //   tripScheduleResponse: {
+        //     scheduleId: null,
+        //     scheduleName: "제주도 여행",
+        //     startTime: "2024-03-01",
+        //     endTime: "2030-01-10",
+        //   },
+        //   findTripsOnSchedules: [
+        //     {
+        //       tripId: null,
+        //       placeName: "롯데월드2",
+        //       coordinateX: 128.576,
+        //       coordinateY: 35.87,
+        //     },
+        //     {
+        //       tripId: null,
+        //       placeName: "롯데월드3",
+        //       coordinateX: 129.224,
+        //       coordinateY: 35.839,
+        //     },
+        //   ],
+        // };
+        setScheduleName(tripData.tripScheduleResponse.scheduleName);
+        setSchedulePeriod(
+          `${tripData.tripScheduleResponse.startTime} - ${tripData.tripScheduleResponse.endTime}`
+        );
+        setDestinations(tripData.findTripsOnSchedules);
+      } catch (error) {
+        console.error("Failed to fetch planner data:", error);
+      }
+    };
+
+    fetchPlannerData();
+  }, [id]);
+
   useEffect(() => {
-    loadKakaoMapScript(() => initializeMap(destinations, setSelectedDestination));
+    if (destinations.length > 0) {
+      loadKakaoMapScript(() =>
+        initializeMap(destinations, setSelectedDestination)
+      );
+    }
   }, [destinations]);
 
   const loadKakaoMapScript = (callback) => {
     const kakaoMapApiKey = import.meta.env.VITE_KAKAO_MAP_API_KEY;
 
     if (!kakaoMapApiKey) {
-      console.error('Kakao Map API key is missing');
+      console.error("Kakao Map API key is missing");
       return;
     }
 
-    const script = document.createElement('script');
+    const script = document.createElement("script");
     script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${kakaoMapApiKey}&autoload=false`;
     script.async = true;
     script.onload = () => window.kakao.maps.load(callback);
@@ -40,7 +86,7 @@ const PlannerDetails = () => {
   };
 
   const initializeMap = (destinations, setSelectedDestination) => {
-    const container = document.getElementById('map'); 
+    const container = document.getElementById("map");
     const options = {
       center: new window.kakao.maps.LatLng(33.450701, 126.570667),
       level: 3,
@@ -48,7 +94,7 @@ const PlannerDetails = () => {
     const map = new window.kakao.maps.Map(container, options);
 
     const bounds = new window.kakao.maps.LatLngBounds();
-    destinations.forEach(destination => {
+    destinations.forEach((destination) => {
       addMarkerToMap(map, destination, bounds, setSelectedDestination);
     });
 
@@ -56,14 +102,17 @@ const PlannerDetails = () => {
   };
 
   const addMarkerToMap = (map, destination, bounds, setSelectedDestination) => {
-    const markerPosition = new window.kakao.maps.LatLng(destination.y, destination.x);
+    const markerPosition = new window.kakao.maps.LatLng(
+      destination.coordinateY,
+      destination.coordinateX
+    );
     const marker = new window.kakao.maps.Marker({
       position: markerPosition,
       map: map,
     });
 
-    window.kakao.maps.event.addListener(marker, 'click', () => {
-      setSelectedDestination(destination.name);
+    window.kakao.maps.event.addListener(marker, "click", () => {
+      setSelectedDestination(destination.placeName);
     });
 
     bounds.extend(markerPosition);
@@ -79,9 +128,14 @@ const PlannerDetails = () => {
     setDestinations(updatedDestinations);
   };
 
-  const handleDelete = (index) => {
+  const handleDelete = async (index) => {
     const updatedDestinations = destinations.filter((_, i) => i !== index);
-    setDestinations(updatedDestinations);
+    try {
+      await axiosInstance.delete(`planner/schedule/${id}`);
+      setDestinations(updatedDestinations);
+    } catch (e) {
+      console.error("Failed to delete destination:", e);
+    }
   };
 
   const handleEditClick = () => {
@@ -107,8 +161,8 @@ const PlannerDetails = () => {
             일정 수정
           </button>
           <div className="planner-details-header">
-            <h1 className="planner-details-title">{plan.title}</h1>
-            <p className="planner-details-period">{plan.period}</p>
+            <h1 className="planner-details-title">{scheduleName}</h1>
+            <p className="planner-details-period">{schedulePeriod}</p>
           </div>
           <DragDropContext onDragEnd={handleDragEnd}>
             <Droppable droppableId="destinations">
@@ -119,10 +173,18 @@ const PlannerDetails = () => {
                   ref={provided.innerRef}
                 >
                   {destinations.map((destination, index) => (
-                    <Draggable key={destination.name} draggableId={destination.name} index={index}>
+                    <Draggable
+                      key={destination.placeName}
+                      draggableId={destination.placeName}
+                      index={index}
+                    >
                       {(provided) => (
                         <li
-                          className={`planner-destination-item ${selectedDestination === destination.name ? 'selected' : ''}`}
+                          className={`planner-destination-item ${
+                            selectedDestination === destination.placeName
+                              ? "selected"
+                              : ""
+                          }`}
                           ref={provided.innerRef}
                           {...provided.draggableProps}
                         >
@@ -132,9 +194,11 @@ const PlannerDetails = () => {
                           >
                             ☰
                           </span>
-                          <span className="destination-name">{destination.name}</span>
-                          <button 
-                            className="delete-button" 
+                          <span className="destination-name">
+                            {destination.placeName}
+                          </span>
+                          <button
+                            className="delete-button"
                             onClick={() => handleDelete(index)}
                           >
                             <img src={deleteIcon} alt="삭제" />
@@ -159,7 +223,7 @@ const PlannerDetails = () => {
           isOpen={isModalOpen}
           mode="edit"
           title="일정 수정"
-          plan={{ title: plan.title, period: plan.period }}
+          plan={{ title: scheduleName, period: schedulePeriod }}
           onClose={() => setIsModalOpen(false)}
           onSave={handleSave}
         />
