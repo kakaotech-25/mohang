9779275f41840c56c5a0f4333081fcaf38a1d04f
msung99 <msung6924@naver.com>
2024-09-13 16:54:56 +0900

    #186 feat: 여행 일정 등록 레포지토리 테스트 픽스쳐로 전환

--- backend/src/test/java/moheng/planner/domain/TripScheduleRegistryRepositoryTest.java
+++ backend/src/test/java/moheng/planner/domain/TripScheduleRegistryRepositoryTest.java
@@ -1,8 +1,9 @@
 package moheng.planner.domain;
 
+import static moheng.fixture.TripFixture.*;
+import static moheng.fixture.TripScheduleFixtures.*;
 import static moheng.fixture.MemberFixtures.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import moheng.config.slice.RepositoryTestConfig;
 import moheng.member.domain.Member;
@@ -14,8 +15,6 @@ import moheng.trip.domain.repository.TripRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.time.LocalDate;
 
 public class TripScheduleRegistryRepositoryTest extends RepositoryTestConfig {
     @Autowired
@@ -34,17 +33,15 @@ public class TripScheduleRegistryRepositoryTest extends RepositoryTestConfig {
     @Test
     void 세부_여행지의_모든_여행지를_제거한다() {
         // given
-        Member member = memberRepository.save(하온_기존());
-        TripSchedule tripSchedule = tripScheduleRepository.save(new TripSchedule("멤버 일정", LocalDate.of(2020, 8, 1), LocalDate.of(2024, 8, 2), member));
-        Trip trip1 = tripRepository.save(new Trip("여행지1", "장소명", 1L, "설명", "https://image.com", 126.3307690830, 36.5309210243));
-        Trip trip2 = tripRepository.save(new Trip("여행지2", "장소명", 2L, "설명", "https://image.com", 226.3307690830, 46.5309210243));
-        Trip trip3 = tripRepository.save(new Trip("여행지3", "장소명", 3L, "설명", "https://image.com", 326.3307690830, 56.5309210243));
-        tripScheduleRegistryRepository.save(new TripScheduleRegistry(trip1, tripSchedule));
-        tripScheduleRegistryRepository.save(new TripScheduleRegistry(trip2, tripSchedule));
-        tripScheduleRegistryRepository.save(new TripScheduleRegistry(trip3, tripSchedule));
+        Member 하온 = memberRepository.save(하온_기존());
+        TripSchedule 여행_일정1 = tripScheduleRepository.save(여행_일정1_생성(하온));
+        Trip 여행지1 = tripRepository.save(여행지1_생성()); Trip 여행지2 = tripRepository.save(여행지2_생성()); Trip 여행지3 = tripRepository.save(여행지3_생성());
+        tripScheduleRegistryRepository.save(여행_일정_등록_생성(여행지1, 여행_일정1));
+        tripScheduleRegistryRepository.save(여행_일정_등록_생성(여행지2, 여행_일정1));
+        tripScheduleRegistryRepository.save(여행_일정_등록_생성(여행지3, 여행_일정1));
 
         // when
-        tripScheduleRegistryRepository.deleteAllByTripScheduleId(tripSchedule.getId());
+        tripScheduleRegistryRepository.deleteAllByTripScheduleId(여행_일정1.getId());
 
         // then
         long exptected = 0L;
@@ -55,17 +52,15 @@ public class TripScheduleRegistryRepositoryTest extends RepositoryTestConfig {
     @Test
     void 세부_일정내의_특정_여행지를_제거한다() {
         // given
-        Member member = memberRepository.save(하온_기존());
-        TripSchedule tripSchedule = tripScheduleRepository.save(new TripSchedule("멤버 일정", LocalDate.of(2020, 8, 1), LocalDate.of(2024, 8, 2), member));
-        Trip trip1 = tripRepository.save(new Trip("여행지1", "장소명", 1L, "설명", "https://image.com", 126.3307690830, 36.5309210243));
-        Trip trip2 = tripRepository.save(new Trip("여행지2", "장소명", 2L, "설명", "https://image.com", 226.3307690830, 46.5309210243));
-        Trip trip3 = tripRepository.save(new Trip("여행지3", "장소명", 3L, "설명", "https://image.com", 326.3307690830, 56.5309210243));
-        tripScheduleRegistryRepository.save(new TripScheduleRegistry(trip1, tripSchedule));
-        tripScheduleRegistryRepository.save(new TripScheduleRegistry(trip2, tripSchedule));
-        tripScheduleRegistryRepository.save(new TripScheduleRegistry(trip3, tripSchedule));
+        Member 하온 = memberRepository.save(하온_기존());
+        TripSchedule 여행_일정1 = tripScheduleRepository.save(여행_일정1_생성(하온));
+        Trip 여행지1 = tripRepository.save(여행지1_생성()); Trip 여행지2 = tripRepository.save(여행지2_생성()); Trip 여행지3 = tripRepository.save(여행지3_생성());
+        tripScheduleRegistryRepository.save(new TripScheduleRegistry(여행지1, 여행_일정1));
+        tripScheduleRegistryRepository.save(new TripScheduleRegistry(여행지2, 여행_일정1));
+        tripScheduleRegistryRepository.save(new TripScheduleRegistry(여행지3, 여행_일정1));
 
         // when
-        tripScheduleRegistryRepository.deleteByTripIdAndTripScheduleId(trip1.getId(), tripSchedule.getId());
+        tripScheduleRegistryRepository.deleteByTripIdAndTripScheduleId(여행지1.getId(), 여행_일정1.getId());
 
         // then
         long exptected = 2L;
