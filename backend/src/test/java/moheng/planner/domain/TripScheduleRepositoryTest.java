88936c2ba50a8606a41372c141ec8b75cf158c79
msung99 <msung6924@naver.com>
2024-09-13 17:02:33 +0900

    #186 test: 여행 일정 레포지토리 테스트 픽스쳐로 전환

--- backend/src/test/java/moheng/planner/domain/TripScheduleRepositoryTest.java
+++ backend/src/test/java/moheng/planner/domain/TripScheduleRepositoryTest.java
@@ -1,5 +1,7 @@
 package moheng.planner.domain;
 
+import static moheng.fixture.TripFixture.*;
+import static moheng.fixture.TripScheduleFixtures.*;
 import static moheng.fixture.MemberFixtures.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -20,6 +22,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class TripScheduleRepositoryTest extends RepositoryTestConfig {
+
     @Autowired
     private TripScheduleRepository tripScheduleRepository;
 
@@ -36,33 +39,11 @@ public class TripScheduleRepositoryTest extends RepositoryTestConfig {
     @Test
     void 여행_일정을_생성날짜_기준으로_내림차순_정렬한다() {
         // given
-        Member member = memberRepository.save(하온_기존());
-        tripScheduleRepository.save(new TripSchedule("일정1", LocalDate.of(2020, 8, 1), LocalDate.of(2024, 8, 2), member));
-        tripScheduleRepository.save(new TripSchedule("일정3", LocalDate.of(2020, 8, 1), LocalDate.of(2024, 8, 2), member));
-        tripScheduleRepository.save(new TripSchedule("일정2", LocalDate.of(2020, 8, 1), LocalDate.of(2024, 8, 2), member));
+        Member 하온 = memberRepository.save(하온_기존());
+        tripScheduleRepository.save(여행_일정2_생성(하온)); tripScheduleRepository.save(여행_일정3_생성(하온)); tripScheduleRepository.save(여행_일정1_생성(하온));
 
         // when
-        List<TripSchedule> actual = tripScheduleRepository.findByMemberOrderByCreatedAtDesc(member);
-
-        // then
-        assertAll(() -> {
-            assertThat(actual.get(0).getName()).isEqualTo("일정2");
-            assertThat(actual.get(1).getName()).isEqualTo("일정3");
-            assertThat(actual.get(2).getName()).isEqualTo("일정1");
-        });
-    }
-
-    @DisplayName("여행 일정을 시작날짜 기준으로 오름차순 정렬한다.")
-    @Test
-    void 여행_일정을_시작날짜_기준으로_오름차순_정렬한다() {
-        // given
-        Member member = memberRepository.save(하온_기존());
-        tripScheduleRepository.save(new TripSchedule("일정1", LocalDate.of(2020, 8, 1), LocalDate.of(2024, 8, 2), member));
-        tripScheduleRepository.save(new TripSchedule("일정2", LocalDate.of(2022, 8, 3), LocalDate.of(2024, 8, 2), member));
-        tripScheduleRepository.save(new TripSchedule("일정3", LocalDate.of(2021, 8, 2), LocalDate.of(2024, 8, 2), member));
-
-        // when
-        List<TripSchedule> actual = tripScheduleRepository.findByMemberOrderByStartDateAsc(member);
+        List<TripSchedule> actual = tripScheduleRepository.findByMemberOrderByCreatedAtDesc(하온);
 
         // then
         assertAll(() -> {
@@ -72,17 +53,33 @@ public class TripScheduleRepositoryTest extends RepositoryTestConfig {
         });
     }
 
+    @DisplayName("여행 일정을 시작날짜 기준으로 오름차순 정렬한다.")
+    @Test
+    void 여행_일정을_시작날짜_기준으로_오름차순_정렬한다() {
+        // given
+        Member 하온 = memberRepository.save(하온_기존());
+        tripScheduleRepository.save(여행_일정1_생성(하온)); tripScheduleRepository.save(여행_일정2_생성(하온)); tripScheduleRepository.save(여행_일정3_생성(하온));
+
+        // when
+        List<TripSchedule> actual = tripScheduleRepository.findByMemberOrderByStartDateAsc(하온);
+
+        // then
+        assertAll(() -> {
+            assertThat(actual.get(0).getName()).isEqualTo("일정1");
+            assertThat(actual.get(1).getName()).isEqualTo("일정2");
+            assertThat(actual.get(2).getName()).isEqualTo("일정3");
+        });
+    }
+
     @DisplayName("여행 일정을 이름순 기준으로 오름차순 정렬한다.")
     @Test
     void 여행_일정을_이름순_기준으로_오름차순_정렬한다() {
         // given
-        Member member = memberRepository.save(하온_기존());
-        tripScheduleRepository.save(new TripSchedule("가 일정", LocalDate.of(2020, 8, 1), LocalDate.of(2024, 8, 2), member));
-        tripScheduleRepository.save(new TripSchedule("다 일정", LocalDate.of(2020, 8, 1), LocalDate.of(2024, 8, 2), member));
-        tripScheduleRepository.save(new TripSchedule("나 일정", LocalDate.of(2020, 8, 1), LocalDate.of(2024, 8, 2), member));
+        Member 하온 = memberRepository.save(하온_기존());
+        tripScheduleRepository.save(여행_일정_가_생성(하온)); tripScheduleRepository.save(여행_일정_다_생성(하온)); tripScheduleRepository.save(여행_일정_나_생성(하온));
 
         // when
-        List<TripSchedule> actual = tripScheduleRepository.findByMemberOrderByNameAsc(member);
+        List<TripSchedule> actual = tripScheduleRepository.findByMemberOrderByNameAsc(하온);
 
         // then
         assertAll(() -> {
@@ -96,26 +93,27 @@ public class TripScheduleRepositoryTest extends RepositoryTestConfig {
     @Test
     void 맴버의_여행_일정중에_이미_일정이_존재하면_참을_리턴한다() {
         // given
-        Member member = memberRepository.save(하온_기존());
-        tripScheduleRepository.save(new TripSchedule("멤버 일정", LocalDate.of(2020, 8, 1), LocalDate.of(2024, 8, 2), member));
+        Member 하온 = memberRepository.save(하온_기존());
+        tripScheduleRepository.save(여행_일정_가_생성(하온));
 
         // when, then
-        assertTrue(tripScheduleRepository.existsByMemberAndName(member, "멤버 일정"));
+        assertTrue(tripScheduleRepository.existsByMemberAndName(하온, "가 일정"));
     }
 
     @DisplayName("일정에 담겨있는 여행지 리스트를 찾는다.")
     @Test
     void 일정에_담겨있는_여행지_리스트를_찾는다() {
         // given
-        Member member = memberRepository.save(하온_기존());
-        TripSchedule tripSchedule = tripScheduleRepository.save(new TripSchedule("멤버 일정", LocalDate.of(2020, 8, 1), LocalDate.of(2024, 8, 2), member));
-        Trip trip1 = tripRepository.save(new Trip("여행지1", "장소명", 1L, "설명", "https://image.com", 126.3307690830, 36.5309210243));
-        Trip trip2 = tripRepository.save(new Trip("여행지2", "장소명", 1L, "설명", "https://image.com", 226.3307690830, 46.5309210243));
-        tripScheduleRegistryRepository.save(new TripScheduleRegistry(trip1, tripSchedule));
-        tripScheduleRegistryRepository.save(new TripScheduleRegistry(trip2, tripSchedule));
+        Member 하온 = memberRepository.save(하온_기존());
+        Trip 여행지1 = tripRepository.save(여행지1_생성()); Trip 여행지2 = tripRepository.save(여행지2_생성());
+
+        TripSchedule 여행_일정1 = tripScheduleRepository.save(여행_일정1_생성(하온));
+
+        tripScheduleRegistryRepository.save(new TripScheduleRegistry(여행지1, 여행_일정1));
+        tripScheduleRegistryRepository.save(new TripScheduleRegistry(여행지2, 여행_일정1));
 
         // when
-        List<Trip> trips = tripRepository.findTripsByScheduleId(tripSchedule.getId());
+        List<Trip> trips = tripRepository.findTripsByScheduleId(여행_일정1.getId());
 
         // then
         assertThat(trips.size()).isEqualTo(2);
