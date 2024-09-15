709ea68c7070362313b3b9ff753600486b1e6efe
jaejunday <wowns8114@gmail.com>
2024-09-15 15:14:31 +0900

    #179 remove: 컴포즈파일의  미사용 frontend override 삭제

--- backend/run_server.sh
+++ backend/run_server.sh
@@ -3,4 +3,4 @@
 # ./gradlew bootRun
 ./gradlew clean bootJar
 
-java -jar -Dspring.profiles.active=dev build/libs/demo-0.0.1-SNAPSHOT.jar
+java -jar -Dspring.profiles.active=local build/libs/demo-0.0.1-SNAPSHOT.jar
