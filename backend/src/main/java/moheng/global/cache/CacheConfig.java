d2e60eaa5a22520881942a1d3ccf94bae498a5f3
msung99 <msung6924@naver.com>
2024-09-29 10:54:24 +0900

    #425 feat: 캐시매니저에 구현체 등록

--- backend/src/main/java/moheng/global/cache/CacheConfig.java
+++ backend/src/main/java/moheng/global/cache/CacheConfig.java
@@ -1,10 +1,23 @@
 package moheng.global.cache;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
     public static final String EXTERNAL_SIMILAR_TRIP_CACHE = "SIMILAR_TRIP_CACHE";
+    private static final long LIFE_CYCLE = 60 * 60;
+
+    @Bean
+    public CacheManager cacheManager() {
+        SimpleCacheManager simpleCacheManager = new SimpleCacheManager();
+        simpleCacheManager.setCaches(List.of(new ExternalSimilarTripCache(EXTERNAL_SIMILAR_TRIP_CACHE, LIFE_CYCLE)));
+        return simpleCacheManager;
+    }
 }
