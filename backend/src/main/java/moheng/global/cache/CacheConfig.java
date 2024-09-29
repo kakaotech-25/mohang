2114a1be86b187471f3b608b9d8a8522bc711956
msung99 <msung6924@naver.com>
2024-09-29 11:18:13 +0900

    #425 feat: 스캐쥴링 활성화

--- backend/src/main/java/moheng/global/cache/CacheConfig.java
+++ backend/src/main/java/moheng/global/cache/CacheConfig.java
@@ -5,12 +5,14 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.List;
 
 @Configuration
 @EnableCaching
+@EnableScheduling
 public class CacheConfig {
     public static final String EXTERNAL_SIMILAR_TRIP_CACHE = "SIMILAR_TRIP_CACHE";
     private static final long LIFE_CYCLE = 60 * 60 * 2;
