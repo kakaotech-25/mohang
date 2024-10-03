82a2e614eada71244bd202ac85c236c29c72b014
msung99 <msung6924@naver.com>
2024-10-03 15:50:58 +0900

    #434 feat: 라우팅 분산

--- backend/src/main/java/moheng/global/replication/DataSourceConfig.java
+++ backend/src/main/java/moheng/global/replication/DataSourceConfig.java
@@ -6,11 +6,13 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
 
+@Profile({"dev"})
 @Configuration
 public class DataSourceConfig {
     private static final String source = "source";
