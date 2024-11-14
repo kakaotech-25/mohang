3ca47f9c6d0bfa20bfb2ea69722ac974346a38bf
msung99 <msung6924@naver.com>
2024-11-15 00:18:02 +0900

    #476 feat: 생활정보 도메인 Validation 추가

--- backend/src/main/java/moheng/liveinformation/dto/request/LiveInformationCreateRequest.java
+++ backend/src/main/java/moheng/liveinformation/dto/request/LiveInformationCreateRequest.java
@@ -1,6 +1,10 @@
 package moheng.liveinformation.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public class LiveInformationCreateRequest {
+
+    @NotEmpty(message = "생활정보 이름은 공백일 수 없습니다.")
     private String name;
 
     private LiveInformationCreateRequest() {
--- backend/src/main/java/moheng/liveinformation/dto/request/UpdateMemberLiveInformationRequest.java
+++ backend/src/main/java/moheng/liveinformation/dto/request/UpdateMemberLiveInformationRequest.java
@@ -1,8 +1,12 @@
 package moheng.liveinformation.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
+
 import java.util.List;
 
 public class UpdateMemberLiveInformationRequest {
+
+    @NotEmpty(message = "업데이트 할 생활정보는 비어있을 수 없습니다.")
     private List<Long> liveInfoIds;
 
     private UpdateMemberLiveInformationRequest() {
