79c99ecf71718305d5d0c3aa9cee0dfbfdcfbbfe
msung99 <msung6924@naver.com>
2024-11-14 23:22:24 +0900

    #476 feat: Auth 도메인 Validation 추가

--- backend/src/main/java/moheng/auth/dto/request/TokenRequest.java
+++ backend/src/main/java/moheng/auth/dto/request/TokenRequest.java
@@ -1,6 +1,9 @@
 package moheng.auth.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 public class TokenRequest {
+    @NotNull(message = "리프레시 토큰은 공백일 수 없습니다.")
     private String code;
 
     private TokenRequest() {}
