e402cb1cc5335e9a0cfbefc5eedf560fbcc57f35
msung99 <msung6924@naver.com>
2024-11-14 22:20:23 +0900

    #477 feat: ErrorResponse 응답 메시지 필드 추가

--- backend/src/main/java/moheng/global/error/ControllerAdvice.java
+++ backend/src/main/java/moheng/global/error/ControllerAdvice.java
@@ -117,7 +117,7 @@ public class ControllerAdvice {
     })
     public ResponseEntity<ExceptionResponse> handleNoResourceFoundException(final NoResourceFoundException e) {
         logger.error(e.getMessage(), e);
-        ExceptionResponse exceptionResponse = new ExceptionResponse("존재하지 않는 리소스 또는 URI 입니다.");
+        ExceptionResponse exceptionResponse = new ExceptionResponse("존재하지 않는 리소스 또는 URI 입니다.", e.getMessage());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionResponse);
     }
@@ -145,20 +145,20 @@ public class ControllerAdvice {
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<ExceptionResponse> handleInvalidRequestBody() {
-        ExceptionResponse exceptionResponse = new ExceptionResponse("잘못된 Request Body 형식 입니다.");
+    public ResponseEntity<ExceptionResponse> handleInvalidRequestBody(final HttpMessageNotReadableException e) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse("잘못된 Request Body 형식 입니다.", e.getMessage());
         return ResponseEntity.badRequest().body(exceptionResponse);
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ResponseEntity<ExceptionResponse> handleNotSupportedMethod() {
-        ExceptionResponse errorResponse = new ExceptionResponse("잘못된 HTTP 메소드 요청입니다.");
+    public ResponseEntity<ExceptionResponse> handleNotSupportedMethod(final HttpRequestMethodNotSupportedException e) {
+        ExceptionResponse errorResponse = new ExceptionResponse("잘못된 HTTP 메소드 요청입니다.", e.getMessage());
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(errorResponse);
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<ExceptionResponse> handleTypeMismatch() {
-        ExceptionResponse exceptionResponse = new ExceptionResponse("잘못된 타입을 가진 데이터가 포함되어 있습니다.");
+    public ResponseEntity<ExceptionResponse> handleTypeMismatch(MethodArgumentTypeMismatchException e) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse("잘못된 타입을 가진 데이터가 포함되어 있습니다.", e.getMessage());
         return ResponseEntity.badRequest().body(exceptionResponse);
     }
 
@@ -167,6 +167,6 @@ public class ControllerAdvice {
         logger.error(e.getMessage(), e);
 
         return ResponseEntity.internalServerError()
-                .body(new ExceptionResponse("서버에 예기치 못한 오류가 발생했습니다. 관리자에게 문의하세요."));
+                .body(new ExceptionResponse("서버에 예기치 못한 오류가 발생했습니다. 관리자에게 문의하세요.", e.getMessage()));
     }
 }
