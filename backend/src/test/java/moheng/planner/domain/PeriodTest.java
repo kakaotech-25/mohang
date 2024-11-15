2aeb5fbc82a800487f9626c25dfb98c3f50efb89
msung99 <msung6924@naver.com>
2024-11-15 14:00:55 +0900

    #482 test: 년도와 월이 모두 동일해야 같은 월에 속하는 날짜로 판별하는지 테스트

--- backend/src/test/java/moheng/planner/domain/PeriodTest.java
+++ backend/src/test/java/moheng/planner/domain/PeriodTest.java
@@ -1,12 +1,16 @@
 package moheng.planner.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import moheng.planner.exception.InvalidDateSequenceException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.LocalDate;
 
@@ -34,4 +38,25 @@ public class PeriodTest {
         assertThatThrownBy(() -> new Period(startDate, endDate))
                 .isInstanceOf(InvalidDateSequenceException.class);
     }
+
+    @DisplayName("년도와 월이 모두 동일해야 같은 월에 속하는 날짜로 판별한다.")
+    @ParameterizedTest
+    @CsvSource({
+            "2024-11-01, 2024-12-01, true",
+            "2024-11-01, 2024-11-30, false",
+            "2023-12-31, 2024-01-01, true"
+    })
+    void 년도와_월이_모두_동일해야_같은_월에_속하는_날짜로_판별한다(final String startDateStr, final String endDateStr, final boolean expected) {
+        // given
+        LocalDate startDate = LocalDate.parse(startDateStr);
+        LocalDate endDate = LocalDate.parse(endDateStr);
+        Period period = new Period(startDate, endDate);
+
+        // when
+        boolean result = period.isOtherMonthDate();
+
+        // then
+        assertEquals(expected, result);
+    }
+
 }
