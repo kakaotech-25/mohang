b4d0c5b010461d6ace9ea4af1adfc77ae265c6ea
msung99 <msung6924@naver.com>
2024-11-15 13:52:29 +0900

    #482 test: 시작날짜가 종료날짜 이후라면 예외가 발생하는지 테스트

--- backend/src/test/java/moheng/planner/domain/PeriodTest.java
+++ backend/src/test/java/moheng/planner/domain/PeriodTest.java
@@ -3,6 +3,8 @@ package moheng.planner.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import moheng.planner.exception.InvalidDateSequenceException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +21,17 @@ public class PeriodTest {
 
         // when, then
         assertDoesNotThrow(() -> new Period(startDate, endDate));
+    }
+
+    @DisplayName("시작날짜가 종료날짜 이후라면 예외가 발생한다.")
+    @Test
+    void 시작날짜가_종료날짜_이후라면_예외가_발생한다() {
+        // given
+        LocalDate startDate = LocalDate.of(2100, 12, 31);
+        LocalDate endDate = LocalDate.of(1800, 1, 1);
+
+        // when, then
+        assertThatThrownBy(() -> new Period(startDate, endDate))
+                .isInstanceOf(InvalidDateSequenceException.class);
     }
 }
