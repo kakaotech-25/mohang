8734c2911c9b5264fe14eece5fb0bfffd9f3b801
msung99 <msung6924@naver.com>
2024-11-15 13:34:57 +0900

    #482 feat: 시작날짜는 종료날짜 날짜 비교 유효성 체크

--- backend/src/main/java/moheng/planner/domain/Period.java
+++ backend/src/main/java/moheng/planner/domain/Period.java
@@ -1,5 +1,7 @@
 package moheng.planner.domain;
 
+import moheng.planner.exception.InvalidDateSequenceException;
+
 import java.time.LocalDate;
 
 public class Period {
@@ -7,8 +9,15 @@ public class Period {
     private final LocalDate endDate;
 
     public Period(final LocalDate startDate, final LocalDate endDate) {
+        validateDateSequence();
         this.startDate = startDate;
         this.endDate = endDate;
+    }
+
+    public void validateDateSequence() {
+        if(startDate.isAfter(endDate)) {
+            throw new InvalidDateSequenceException("시작날짜는 종료날짜보다 더 이후일 수 없습니다.");
+        }
     }
 
     public boolean isOtherMonthDate() {
---
+++ backend/src/main/java/moheng/planner/exception/InvalidDateSequenceException.java
@@ -0,0 +1,7 @@
+package moheng.planner.exception;
+
+public class InvalidDateSequenceException extends RuntimeException {
+    public InvalidDateSequenceException(String message) {
+        super(message);
+    }
+}
