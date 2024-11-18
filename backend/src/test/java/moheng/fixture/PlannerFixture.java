3c61a9f2c713eb1e7a18a810ccd376faa5781ee4
msung99 <msung6924@naver.com>
2024-11-18 10:17:16 +0900

    #484 chore: 요청, 응답 Fixture DTO 모으기

--- backend/src/test/java/moheng/fixture/PlannerFixture.java
+++ backend/src/test/java/moheng/fixture/PlannerFixture.java
@@ -24,6 +24,18 @@ public class PlannerFixture {
         );
     }
 
+    public static FindPublicSchedulesForRangeRequest 플래너_생성날짜_기준_범위_내의_공개된_여행지_조회_요청() {
+        return new FindPublicSchedulesForRangeRequest(LocalDate.of(2020, 1, 1), LocalDate.of(2020, 3, 10));
+    }
+
+    public static FindPublicSchedulesForRangeRequest 잘못된_시작날짜_종료날짜로_플래너_생성날짜_기준_범위_내의_공개된_여행지_조회_요청() {
+        return new FindPublicSchedulesForRangeRequest(LocalDate.of(2020, 3, 1), LocalDate.of(2020, 1, 10));
+    }
+
+    public static FindSchedulesByNameRequest 여행_일정_검색명_조회_요청() {
+        return new FindSchedulesByNameRequest("제주도");
+    }
+
     // 플래너 응답
     public static FindPlannerOrderByRecentResponse 플래너_최신순_조회_응답() {
         return new FindPlannerOrderByRecentResponse(List.of(
@@ -66,17 +78,5 @@ public class PlannerFixture {
                 new TripSchedule("제주도 여행 일정", LocalDate.of(2024, 3, 10), LocalDate.of(2030, 1, 10), false, 하온_기존()),
                 new TripSchedule("제주도 가자!", LocalDate.of(2023, 2, 15), LocalDate.of(2030, 1, 10), false, 하온_기존())
         ));
-    }
-
-    public static FindPublicSchedulesForRangeRequest 플래너_생성날짜_기준_범위_내의_공개된_여행지_조회_요청() {
-        return new FindPublicSchedulesForRangeRequest(LocalDate.of(2020, 1, 1), LocalDate.of(2020, 3, 10));
-    }
-
-    public static FindPublicSchedulesForRangeRequest 잘못된_시작날짜_종료날짜로_플래너_생성날짜_기준_범위_내의_공개된_여행지_조회_요청() {
-        return new FindPublicSchedulesForRangeRequest(LocalDate.of(2020, 3, 1), LocalDate.of(2020, 1, 10));
-    }
-
-    public static FindSchedulesByNameRequest 여행_일정_검색명_조회_요청() {
-        return new FindSchedulesByNameRequest("제주도");
     }
 }
